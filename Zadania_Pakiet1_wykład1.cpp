97360d3cabdd1ea7eb2b1899e080e960085f83e3
Cyprian <Cyprian.Gibas@o2.pl>
2023-09-24 20:35:09 +0200

    zad 2

--- Zadania_Pakiet1_wykład1.cpp
+++ Zadania_Pakiet1_wykład1.cpp
@@ -1,7 +1,14 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
-    cout << "OUR"<< endl << "DEMOCRACY" << endl << "HAS BEEN"  <<endl<< "HACKED";
+int main() {
+    // czy w poleceniu pierwsza zmienna nie powinna być typu char? bo miało być 5 różnych typów zmiennych
+
+    int sign = 1;
+    float num1 = 1011001.01001111;
+    int num2 = 1010101;
+    string text = "YOU";
+    bool flag = true;
+
+    cout << sign << endl << num1 << endl << num2 << endl << text << endl << flag;
 }
