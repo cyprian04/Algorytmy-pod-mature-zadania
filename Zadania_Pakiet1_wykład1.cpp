026dea1b8ae06a97458570d843f7dff78bf14b73
Cyprian <Cyprian.Gibas@o2.pl>
2023-09-24 22:15:41 +0200

    zad 3

--- Zadania_Pakiet1_wykład1.cpp
+++ Zadania_Pakiet1_wykład1.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-    // czy w poleceniu pierwsza zmienna nie powinna być typu char? bo miało być 5 różnych typów zmiennych
+    int x, y;
 
-    int sign = 1;
-    float num1 = 1011001.01001111;
-    int num2 = 1010101;
-    string text = "YOU";
-    bool flag = true;
+    cout << "Podaj liczbe x: ";
+    cin >> x;
+    cout << "Podaj liczbe y: ";
+    cin >> y;
 
-    cout << sign << endl << num1 << endl << num2 << endl << text << endl << flag;
+    int result = x % y;
+    cout <<"Reszta z dzielenia liczby "<< x <<" przez "<< y <<" to: "<< result;
 }
