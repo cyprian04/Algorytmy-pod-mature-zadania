f030df22d2bce629cd80625c38521f71e03cf045
Cyprian <Cyprian.Gibas@o2.pl>
2023-09-25 21:35:43 +0200

    zad 9

--- Zadania_Pakiet1_wykład1.cpp
+++ Zadania_Pakiet1_wykład1.cpp
@@ -2,30 +2,26 @@
 using namespace std;
 
 int main() {
-    //teoretycznie pesel powinien być stringiem, ale zrobione na intach
+	int option = 0;
 
-    int pesel[11];
-    int weight[10] = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
-    int Sum = 0;
-    int change = 0;
+	cout <<"= = = Wybierz jedna z opcji, aby obliczyc pole = = ="<< endl;
+	cout <<"1. Kwadratu" << endl <<"2. Prostokata" << endl <<"3. Trojkata" << endl;
+	cout <<"Opcja: ";
+	cin >> option;
 
-    for (int i = 0; i < 11; i++) {
-        cout <<"Podaj "<<i+1<<" cyfre peselu: ";
-        cin >> pesel[i];
-
-        if (i < 10) {
-            Sum += pesel[i] * weight[i];
-        }
-    }
-    change = Sum % 10;
-
-    if ((change != 0) && (10 - change) == pesel[10]) {
-        cout <<"Pesel jest poprawny :)";
-    }
-    else if (change == pesel[10]) {
-        cout <<"Pesel jest poprawny :)";
-    }
-    else {
-        cout <<"Pesel nie jest poprawny :(";
-    }
+	if (option >= 1 && option <= 3) {	
+		cout << "Wybrano obliczanie pola ";
+		if (option == 1){
+			cout <<"kwadratu";
+		}
+		else if (option == 2) {
+			cout << "prostokata";
+		}
+		else{
+			cout << "trojkata";
+		}
+	}
+	else {
+		cout << "Podano zla opcje";
+	}
 }
