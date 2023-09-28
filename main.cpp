dd008b9531f22c29e9663fb1892f91181b20f9b1
Cyprian <Cyprian.Gibas@o2.pl>
2023-09-28 16:58:58 +0200

    zad 5

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 ﻿#include <iostream>
-
+// zrozumiałem żeby tak wyświetlać sumę
 int main() {
-	int i = -9;
-	while (i < 40) {
-		std::cout << i << std::endl;
-		i+=2;
-	}
+	int sum = 0;
+
+	for (int i = 1; i <= 1000; i++)
+		if ((i % 5 == 0) && (i % 3 == 0)) {
+			sum += i;
+			std::cout << i <<" Suma: "<< sum <<std::endl;
+		}
 	return 0;
 }
