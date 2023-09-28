c20e1dcd65bf99e62b7cd187ac261efa0e4664e0
Cyprian <Cyprian.Gibas@o2.pl>
2023-09-28 17:22:16 +0200

    poprawione

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
 	std::cout << "Podaj liczbe dodatnia, wieksza od 1: ";
 	std::cin >> num;
 
-	for (int i = 1; i < num ; i++)
+	for (int i = 1; i <= num ; i++)
 		if (i % 2 != 0)
 			std::cout << i <<" | ";
 	return 0;
