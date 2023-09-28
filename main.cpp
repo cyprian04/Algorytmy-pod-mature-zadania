65d57f00a936e942a045dfb233c6aa45a0a08eaa
Cyprian <Cyprian.Gibas@o2.pl>
2023-09-28 17:29:05 +0200

    zad 8

--- main.cpp
+++ main.cpp
@@ -2,11 +2,10 @@
 
 int main() {
 	int num = 0;
-	std::cout << "Podaj liczbe dodatnia: ";
+	std::cout << "Podaj liczbe dodatnia, wieksza od 1: ";
 	std::cin >> num;
 
-	for (int i = 1; i <= num ; i++)
-		if (i % 2 != 0)
+	for (int i = 2; i <= num ; i*=2)
 			std::cout << i <<" | ";
 	return 0;
 }
