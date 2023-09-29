c1cd9e1f0cd64a83c9de952521d705da738e63d8
Cyprian <Cyprian.Gibas@o2.pl>
2023-09-29 21:44:47 +0200

    zad 18

--- main.cpp
+++ main.cpp
@@ -3,24 +3,14 @@
 int main() {
 	int num = 0;
 	int digit = 0;
-	int even = 0;
-	int odd = 0;
 	std::cout << "Podaj liczbe naturalna: ";
 	std::cin >> num;
 
 	while (num != 0) {
 		digit = num % 10;
-		if (digit % 2 == 0) even++;
-		else odd++;
+		std::cout << digit;
 		num -= digit;
 		num /= 10;
 	}
-
-	if(even > odd)
-		std::cout << "wiecej jest parzystych";
-	else if (even < odd)
-		std::cout << "wiecej jest nieparzystych";
-	else
-		std::cout << "jest tyle samo parzystch co nieparzystych";
 	return 0;
 }
