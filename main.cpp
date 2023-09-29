658d68c99d74340cf632ad6546c4363b9396d5f8
Cyprian <Cyprian.Gibas@o2.pl>
2023-09-29 20:51:35 +0200

    zad 13

--- main.cpp
+++ main.cpp
@@ -4,16 +4,16 @@ int main() {
 	int num = 0;
 	int digit = 0;
 	int total = 0;
-	std::cout << "Podaj liczbe: ";
+	std::cout << "Podaj liczbe naturalna: ";
 	std::cin >> num;
 
 	while (num != 0){
 		digit = num % 10;
-		if (digit % 2 != 0)
-			total += digit;
 		num -= digit;
 		num /= 10;
+		total++;
 	}
+	if (total == 0) total++;
 	std::cout << total;
 
 	return 0;
