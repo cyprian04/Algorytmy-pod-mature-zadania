c229bbd9baaf96a7c3bd19a850c2ded4912b44fc
Cyprian <Cyprian.Gibas@o2.pl>
2023-09-29 21:18:36 +0200

    zad 16

--- main.cpp
+++ main.cpp
@@ -8,12 +8,18 @@ int main() {
 	std::cin >> num;
 
 	while (num != 0){
+		bool isPrime = true;
 		digit = num % 10;
+
+		for (int i = 2; i < digit; i++)
+			if (digit % i == 0) {
+				isPrime = false;
+				break;
+			}
+		if (isPrime) total++;
 		num -= digit;
 		num /= 10;
-		total++;
 	}
-	if (total == 0) total++;
 	std::cout << total;
 
 	return 0;
