aab9e472c076ae838004dc359034e15c2a021f0e
Cyprian <Cyprian.Gibas@o2.pl>
2023-09-29 19:51:56 +0200

    zad 12

--- main.cpp
+++ main.cpp
@@ -3,14 +3,18 @@
 int main() {
 	int num = 0;
 	int digit = 0;
+	int total = 0;
 	std::cout << "Podaj liczbe: ";
 	std::cin >> num;
 
 	while (num != 0){
 		digit = num % 10;
-		std::cout << digit * digit << " | ";
+		if (digit % 2 != 0)
+			total += digit;
 		num -= digit;
 		num /= 10;
 	}
+	std::cout << total;
+
 	return 0;
 }
