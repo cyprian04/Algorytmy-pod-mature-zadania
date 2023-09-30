4aa920ebc6cadb80d3b2d71079bab563108aff73
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-09-30 21:03:52 +0200

    zad 6

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 ﻿#include <iostream>
 int main() {
-	for (char a = 'a'; a <= 'z'; a++)
-		std::cout << a<<" ";
-
-	std::cout<<"\n";
-	for (char a = 'A'; a <= 'Z'; a++)
-		std::cout << a << " ";
+	std::string text;
+	bool flag = true;
+	std::cout << "Podaj wyraz: ";
+	std::cin >> text;
+	for (int i = 0; i < text.length(); i++)
+		if (text[i] != text[text.length() -1 -i]){
+			std::cout << "Nie jest palindromem";
+			return 0;
+		}
+	if (flag)
+		std::cout << "jest palindromem";
 	return 0;
 }
