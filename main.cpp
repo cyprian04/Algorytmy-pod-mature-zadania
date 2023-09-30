01b26df4a6dbef7a4c2a0b4aeedac91628f9c29f
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-09-30 19:08:48 +0200

    zad 4

--- main.cpp
+++ main.cpp
@@ -1,16 +1,31 @@
 ﻿#include <iostream>
+// alokacja na heap'ie, zrozumiałem że wartości mają być pod koniec dlatego jest 2 for :)
 int main() {
-	int a1, r, n, an;
+	int* nums = nullptr;
+	int size = 0;
 
-	std::cout << "Podaj wartosc pierwszego wyrazu ciagu arytmetycznego: ";
-	std::cin >> a1;
-	std::cout << "Podaj r ciagu arytmetycznego: ";
-	std::cin >> r;
-	std::cout << "Podaj n-ty wyraz do obliczenia: ";
-	std::cin >> n;
+	std::cout << "Podaj ile liczb chcesz sprawdzic, ktore sa liczbami pierwszymi: ";
+	std::cin >> size;
+	nums = new int[size]; 
 
-	an = a1 + (n - 1) * r;
-	std::cout <<"wartosc wyrazu a"<< n <<" = "<< an;
-	std::cout << "\nwartosc sumy pierwszych " << n << " wyrazow = " << ((a1 + an) / 2) * n;
+	for (int i = 0; i < size; i++) {
+		std::cout << "Podaj liczbe :";
+		std::cin >> nums[i];
+	}
+
+	for (int i = 0; i < size; i++) {
+		bool isPrime = true;
+		for (int j = 2; j < nums[i]; j++)
+			if (nums[i] % j == 0) {
+				isPrime = false;
+				break;
+			}
+		if (isPrime)
+			std::cout << nums[i] << " jest pierwsza\n";
+		else
+			std::cout << nums[i] << " NIE jest pierwsza\n";
+	}
+	delete[] nums; // usuniecie z heapa
+	nums = nullptr;
 	return 0;
 }
