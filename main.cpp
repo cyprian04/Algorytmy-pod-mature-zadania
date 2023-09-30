f5c9591f2eda08d2b09e2bc6d4971bd43b0ffc4f
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-09-30 22:00:00 +0200

    zad 9

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 ﻿#include <iostream>
 int main() {
+	int count = 0;
 	std::string text;
 	std::cout << "Podaj wyraz: ";
 	std::cin >> text;
+
 	for (size_t i = 0; i < text.length(); i++)
-		if (text[i] != text[text.length() - 1 - i]) {
-			std::cout << "Nie jest palindromem";
-			return 0;
-		}
-	std::cout << "jest palindromem";
+		if (text[i] == 'a' || text[i] == 'A')
+			count++;
+	std::cout << "Wyraz " <<text<<" ma "<< count <<" a";
 	return 0;
 }
