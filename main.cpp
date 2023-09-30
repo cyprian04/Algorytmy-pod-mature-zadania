e781bc79113387a33f005fc3971dfdad9295eab0
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-09-30 13:03:56 +0200

    zad 1

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 ﻿#include <iostream>
 
 int main() {
-	int num = 0;
-	int digit = 0;
-	std::cout << "Podaj liczbe naturalna: ";
-	std::cin >> num;
+	int array[10];
 
-	while (num != 0) {
-		digit = num % 10;
-		std::cout << digit;
-		num -= digit;
-		num /= 10;
-	}
+	for (int i = 0; i < 10; i++) {
+		std::cout << "Podaj liczbe numer " <<i+1<<": ";
+		std::cin >> array[i];
+	}	
+	for (int i = 9; i >= 0 ; i--)
+		std::cout << array[i] <<" ";
 	return 0;
 }
