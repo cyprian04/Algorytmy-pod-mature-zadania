f1b65d289cdb9edf1ce4266d32fbed393c4506f9
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-09-30 21:47:08 +0200

    zad 6 poprawione

--- main.cpp
+++ main.cpp
@@ -1,31 +1,13 @@
 ﻿#include <iostream>
-#include <vector>
-// dla odmiany użycie vectora :)
 int main() {
-	int vecSize = 0;
 	std::string text;
-	std::vector<std::string> strings;
-
-	std::cout << "Podaj ilosc wyrazow do sprawdzenia: ";
-	std::cin >> vecSize;
-
-	for (size_t i = 0; i < vecSize; i++) {
-		std::cout <<"Podaj wyraz: ";
-		std::cin >> text;
-		strings.push_back(text);
-	}
-
-	for (const auto& text : strings){
-		bool flag = true;
-		for (size_t i = 0; i < text.length(); i++)
-			if (text[i] != text[text.length() -1 -i]){
-				flag = false;
-				break;
-			}
-		if (flag)
-			std::cout << "Jest palindromem\n";
-		else
-			std::cout << "Nie Jest palindromem\n";
-	}
+	std::cout << "Podaj wyraz: ";
+	std::cin >> text;
+	for (size_t i = 0; i < text.length(); i++)
+		if (text[i] != text[text.length() - 1 - i]) {
+			std::cout << "Nie jest palindromem";
+			return 0;
+		}
+	std::cout << "jest palindromem";
 	return 0;
 }
