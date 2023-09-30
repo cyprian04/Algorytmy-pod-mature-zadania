7c4b4e580b3beceb3f4d0f2bc0deb9adcc6d203b
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-09-30 19:12:37 +0200

    zad 5

--- main.cpp
+++ main.cpp
@@ -1,31 +1,10 @@
 ﻿#include <iostream>
-// alokacja na heap'ie, zrozumiałem że wartości mają być pod koniec dlatego jest 2 for :)
 int main() {
-	int* nums = nullptr;
-	int size = 0;
+	for (char a = 'a'; a <= 'z'; a++)
+		std::cout << a<<" ";
 
-	std::cout << "Podaj ile liczb chcesz sprawdzic, ktore sa liczbami pierwszymi: ";
-	std::cin >> size;
-	nums = new int[size]; 
-
-	for (int i = 0; i < size; i++) {
-		std::cout << "Podaj liczbe :";
-		std::cin >> nums[i];
-	}
-
-	for (int i = 0; i < size; i++) {
-		bool isPrime = true;
-		for (int j = 2; j < nums[i]; j++)
-			if (nums[i] % j == 0) {
-				isPrime = false;
-				break;
-			}
-		if (isPrime)
-			std::cout << nums[i] << " jest pierwsza\n";
-		else
-			std::cout << nums[i] << " NIE jest pierwsza\n";
-	}
-	delete[] nums; // usuniecie z heapa
-	nums = nullptr;
+	std::cout<<"\n";
+	for (char a = 'A'; a <= 'Z'; a++)
+		std::cout << a << " ";
 	return 0;
 }
