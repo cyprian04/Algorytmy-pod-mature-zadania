202eac3fe54599986fc202b3fd51d561fa1f8d53
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-02 19:28:09 +0200

    zad 12

--- main.cpp
+++ main.cpp
@@ -17,14 +17,14 @@ int main() {
 		std::cin >> arr[i];
 	}
 
-	for (int i = 0; i < size - 1 && currentShift < shiftCount;) {
-		std::swap(arr[i + 1], arr[i]);
-		if (i == size - 2) {
-			i = 0;
+	for (int i = size - 1; i > 0 && currentShift < shiftCount;) {
+		std::swap(arr[i - 1], arr[i]);
+		if (i == 1) {
+			i = size - 1;
 			currentShift++;
 			continue;
 		}
-		i++;
+		i--;
 	}
 
 	for (int i = 0; i < size; i++)
