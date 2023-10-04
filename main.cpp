59ce164dc5b83dd5124f0d3ee88c9f56f191cc43
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-04 17:27:06 +0200

    zad 4

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 ﻿#include <iostream>
 
-bool isInRange(const int num1, const int num2, const int num3) {
-	return (num1 >= 13 && num1 <= 19) || (num2 >= 13 && num2 <= 19) || (num3 >= 13 && num3 <= 19);
+bool isPalindrom(const std::string text_in) {
+	for (int i = 0; i < text_in.length(); i++)
+		if (text_in[i] != text_in[text_in.length() - 1 - i])
+			return false;
+	return true;
 }
 
 int main() {
-	int nums[3] = {0};
+	std::string text;
+	std::cout << "Podaj wyraz: ";
+	std::cin >> text;
 
-	for (int i = 0; i < 3; i++) {
-		std::cout << "Podaj liczbe "<< i+1 <<": ";
-		std::cin >>nums[i];
-	}
-
-	if (isInRange(nums[0], nums[1], nums[2]))
-		std::cout << "Co najmniej jedna z nich nalezy do przedzialu od 13 do 19";
+	if (isPalindrom(text))
+		std::cout << "Jest palindromem";
 	else
-		std::cout << "Zadna z nich nie nalezy do przedzialu od 13 do 19";
+		std::cout << "Nie jest palindromem";
 	return 0;
 }
