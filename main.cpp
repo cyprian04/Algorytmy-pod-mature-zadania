ed2b5d72c3d482c7d2f4170182c2545886fbc844
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-04 20:19:05 +0200

    zad 15

--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
 ﻿#include <iostream>
 
-int Power1(int num, int pow) {
-    int result = 1;
-
-    if (num >= 0)
-        for (int i = 0; i < pow; i++)
-            result *= num;
-    else
-        return 0;
-
-    return result;
+int Power2(int x, int n) {
+    if (n == 0)
+        return 1;
+    else if (n > 0)
+        return x * Power2(x, n - 1);
 }
 
 int main() {
@@ -20,7 +15,7 @@ int main() {
     std::cin >> num;
     std::cout << "Podaj potege tej liczby do obliczenia: ";
     std::cin >> pow;
-    std::cout << "Wynik: "<< Power1(num, pow);
+    std::cout << "Wynik: "<< Power2(num, pow);
 
     return 0;
 }
