e6ef3dacf2be508df1383b50ca256d1601748ad8
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-04 19:05:31 +0200

    zad 11

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 ﻿#include <iostream>
 
 long long CountTheFactorial(int num) {
-    long long result = 1;
-    for (int i = num; i > 0; i--)
-         result*=i;
-    return result;
+    if (num == 1)
+        return num;
+    return CountTheFactorial(num-1)*num; 
 }
 
 int main() {
     int factorial = 0;
-
     std::cout << "Podaj silnie do wyliczenia: ";
     std::cin >> factorial;
     std::cout << factorial << "! wynosi: " << CountTheFactorial(factorial);
+
     return 0;
 }
