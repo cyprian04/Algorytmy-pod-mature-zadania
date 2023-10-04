fb6cc43d1cd0b1c58f29b2bfb954d90774195e89
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-04 18:24:53 +0200

    zad 7

--- main.cpp
+++ main.cpp
@@ -1,29 +1,25 @@
 ﻿#include <iostream>
 
-void CheckPesel(const std::string pesel_in) {
-    int weight[11] = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1 };
-    int Sum = 0;
-    int change = 0;
+int HowManyDigits(int num) {
+    int sum = 0;
+    int digit = 0;
 
-    for (int i = 0; i < 11; i++)
-        Sum += pesel_in[i] * weight[i];
-    change = Sum % 10;
+    if (num == 0) return ++sum;
 
-    if (change == 0)
-        std::cout << "Pesel jest poprawny :)";
-    else
-        std::cout << "Pesel nie jest poprawny :(";
+    while (num != 0) {
+        digit = num % 10;
+        num -= digit;
+        num /= 10;
+        sum++;
+    }
+    return sum;
 }
 
 int main() {
-    std::string pesel;
-    pesel.resize(11);
-
-    for (size_t i = 0; i < pesel.length(); i++) {
-        std::cout << "Podaj " << i + 1 << " cyfre peselu: ";
-        std::cin >> pesel[i];
-    }
-    CheckPesel(pesel);
+    int num = 0;
+    std::cout << "Podaj liczbe: ";
+    std::cin >> num;
+    std::cout << "ilosc cyfr tej liczby to: " << HowManyDigits(num);
 
     return 0;
 }
