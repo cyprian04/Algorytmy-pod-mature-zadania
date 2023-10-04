da7c3867be34acfda796007b98dbda5d43e2b7c5
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-04 20:12:09 +0200

    zad 14

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 ﻿#include <iostream>
 
-std::string writeLine(std::string sign_in, int n) {
-    if (n > 1)
-        return writeLine(sign_in, n - 1)+sign_in;
-    return sign_in;
+int Power1(int num, int pow) {
+    int result = 1;
+
+    if (num >= 0)
+        for (int i = 0; i < pow; i++)
+            result *= num;
+    else
+        return 0;
+
+    return result;
 }
 
 int main() {
-    std::string sign;
     int num = 0;
+    int pow = 0;
 
-    std::cout << "Podaj znak: ";
-    std::cin >> sign;
-    std::cout << "Podaj ilosc razy do wypisania tego znaku: ";
+    std::cout << "Podaj liczbe: ";
     std::cin >> num;
-    std::cout << writeLine(sign, num);
+    std::cout << "Podaj potege tej liczby do obliczenia: ";
+    std::cin >> pow;
+    std::cout << "Wynik: "<< Power1(num, pow);
 
     return 0;
 }
