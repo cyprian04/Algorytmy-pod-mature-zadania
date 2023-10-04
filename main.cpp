a4cf9a6e0ee520d55a8ad6d4d2e0b9d763a460ef
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-04 20:02:44 +0200

    zad 13

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 ﻿#include <iostream>
 
-int Fib(int n) {
-    if (n == 0)
-        return 0;
-    else if (n == 1)
-        return 1;
-    return Fib(n - 1) + Fib(n - 2); 
+std::string writeLine(std::string sign_in, int n) {
+    if (n > 1)
+        return writeLine(sign_in, n - 1)+sign_in;
+    return sign_in;
 }
 
 int main() {
+    std::string sign;
     int num = 0;
-    std::cout << "Podaj n-ty wyrazu ciagu Fibonacciego do obliczenia: ";
+
+    std::cout << "Podaj znak: ";
+    std::cin >> sign;
+    std::cout << "Podaj ilosc razy do wypisania tego znaku: ";
     std::cin >> num;
-    std::cout << num << " wyraz ciagu ma wartosc: " << Fib(num);
+    std::cout << writeLine(sign, num);
 
     return 0;
 }
