cfed902a414f598f39fe7f8bd4bb9b29a7924f7e
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-07 11:38:05 +0200

    zad 8 poprawione

--- main.cpp
+++ main.cpp
@@ -1,30 +1,23 @@
 ﻿#include <iostream>
 
-bool CheckPesel(const std::string pesel_in) {
-    int weight[11] = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1 };
-    int Sum = 0;
-    int change = 0;
-
-    for (int i = 0; i < 11; i++)
-        Sum += pesel_in[i] * weight[i];
-    change = Sum % 10;
-
-    return change == 0;
+bool IsPrime(const int num) {
+    bool isPrime = true;
+    for (int i = 2; i < num; i++)
+        if (num % i == 0) {
+            isPrime = false;
+            break;
+        }
+    return isPrime && num != 1;
 }
 
 int main() {
-    std::string pesel;
-    pesel.resize(11);
+    int num = 0;
+    std::cout << "Podaj liczbe: ";
+    std::cin >> num;
 
-    for (size_t i = 0; i < pesel.length(); i++) {
-        std::cout << "Podaj " << i + 1 << " cyfre peselu: ";
-        std::cin >> pesel[i];
-    }
-    
-    if (CheckPesel(pesel))
-        std::cout << "Pesel jest poprawny :)";
+    if (IsPrime(num))
+        std::cout << "Jest pierwsza";
     else
-        std::cout << "Pesel nie jest poprawny :(";
-
+        std::cout << "NIE jest pierwsza";
     return 0;
 }
