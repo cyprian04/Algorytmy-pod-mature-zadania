65f3bc379553568d3029bf95d19b473fe6af72a6
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-07 11:35:13 +0200

    zad 6 poprawione

--- main.cpp
+++ main.cpp
@@ -1,25 +1,30 @@
 ﻿#include <iostream>
-#include <cmath>
-int Power3(int x, int n) {
-    if (n < 1)
-        return 1;
 
-    else if (n % 2 == 0)
-        return std::pow(Power3(x, n / 2), 2);
+bool CheckPesel(const std::string pesel_in) {
+    int weight[11] = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1 };
+    int Sum = 0;
+    int change = 0;
 
-    else if (n % 2 != 0)
-        return x * std::pow(Power3(x, n / 2), 2);
+    for (int i = 0; i < 11; i++)
+        Sum += pesel_in[i] * weight[i];
+    change = Sum % 10;
+
+    return change == 0;
 }
 
 int main() {
-    int num = 0;
-    int pow = 0;
+    std::string pesel;
+    pesel.resize(11);
 
-    std::cout << "Podaj liczbe: ";
-    std::cin >> num;
-    std::cout << "Podaj potege tej liczby do obliczenia: ";
-    std::cin >> pow;
-    std::cout << "Wynik: "<< Power3(num, pow);
+    for (size_t i = 0; i < pesel.length(); i++) {
+        std::cout << "Podaj " << i + 1 << " cyfre peselu: ";
+        std::cin >> pesel[i];
+    }
+    
+    if (CheckPesel(pesel))
+        std::cout << "Pesel jest poprawny :)";
+    else
+        std::cout << "Pesel nie jest poprawny :(";
 
     return 0;
 }
