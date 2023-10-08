abc3e3da483ed261cb1d23ee627132d2ba02baea
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-10-08 12:53:09 +0200

    zad 1

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 ﻿#include <iostream>
 
-long long CountTheFactorial(int num) {
-    if (num == 1 || num == 0)
-        return num;
-    return CountTheFactorial(num - 1) * num;
-}
-
 int main() {
-    int factorial = 0;
-    std::cout << "Podaj silnie do wyliczenia: ";
-    std::cin >> factorial;
-    std::cout << factorial << "! wynosi: " << CountTheFactorial(factorial);
+    int even = 2;
+    const int rowCount = 5;
+    const int columnCount = 5;
+    int tablica[rowCount][columnCount];
 
+    //wpisywanie
+    for (int i = 0; i < rowCount; i++)
+        for (int j = 0; j < columnCount; j++, even+=2)
+             tablica[i][j] = even;
+
+    //wyświetlanie
+    for (int i = 0; i < rowCount; i++) {
+        for (int j = 0; j < columnCount; j++) {
+            std::cout << tablica[i][j] <<" ";
+        }
+        std::cout <<std::endl;
+    }
     return 0;
 }
