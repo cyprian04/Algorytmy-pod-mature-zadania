93c952d3a61574a4becae259daa5a866bda836af
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-08 20:43:42 +0200

    zad 10

--- main.cpp
+++ main.cpp
@@ -1,24 +1,30 @@
 ﻿#include <iostream>
 
 int main() {
-    char sign = 'A'; //65
-    const int rowCount = 5;
-    const int columnCount = 5;
-    char tablica[rowCount][columnCount];
+    char number = 1;
+    const int rowCount = 3;
+    const int columnCount = 3;
+    int tablica[rowCount][columnCount];
+    int pomocnicza[rowCount][columnCount];
 
     for (int i = 0; i < rowCount; i++){
-        for (int j = 0; j < columnCount; j++, sign++){
-             tablica[i][j] = sign;
+        for (int j = 0; j < columnCount; j++, number++){
+             tablica[i][j] = number;
              std::cout << tablica[i][j] << " ";
         }
         std::cout << std::endl;
     }
     std::cout << std::endl;
 
+    for (int i = 0; i < rowCount; i++)
+        for (int j = 0; j < columnCount; j++)
+             pomocnicza[j][rowCount- 1 - i] = tablica[i][j];
+
     for (int i = 0; i < rowCount; i++) {
         for (int j = 0; j < columnCount; j++)
-            std::cout << char(tablica[i][j] + 32) <<" ";
+            std::cout << pomocnicza[i][j] << " ";
         std::cout << std::endl;
     }
+
     return 0;
 }
