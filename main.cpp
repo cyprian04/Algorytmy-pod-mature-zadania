f1c69751b92008a62b983fe857962ddd7480c5aa
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-10-08 14:10:33 +0200

    zad 4

--- main.cpp
+++ main.cpp
@@ -2,20 +2,29 @@
 
 int main() {
     int number = 1;
-    int sum = 1;
-    const int rowCount = 5;
-    const int columnCount = 5;
+    int minElement, maxElement;
+    const int rowCount = 8;
+    const int columnCount = 8;
     int tablica[rowCount][columnCount];
 
-    //wpisywanie
+    for (int i = 0; i < rowCount; i++)
+        for (int j = 0; j < columnCount; j++, number++) 
+             tablica[i][j] = number;
+
+    minElement = tablica[0][0];
+    maxElement = tablica[0][0];
+
     for (int i = 0; i < rowCount; i++) {
-        for (int j = 0; j < columnCount; j++, number++) {
-            tablica[i][j] = number;
-            if (number % 7 == 0)
-                sum *= number;
+        for (int j = 0; j < columnCount; j++) {
+
+            if(tablica[i][j] < minElement)
+                minElement = tablica[i][j];
+            else if(tablica[i][j] > maxElement)
+                maxElement = tablica[i][j];
         }
     }
 
-    std::cout <<"Iloczyn elementow podzielnych przez 7: "<< sum << std::endl;
+    std::cout << "Maksymalny element tablicy to: " << maxElement <<std::endl;
+    std::cout << "Minimalny element tablicy to: " << minElement;
     return 0;
 }
