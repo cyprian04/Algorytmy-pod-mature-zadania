ea6370dd69dec6a73d17e8b83dbaea72e40ba761
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-08 19:52:56 +0200

    zad 8

--- main.cpp
+++ main.cpp
@@ -1,34 +1,34 @@
 ﻿#include <iostream>
 
+bool isPrime(int num) {
+    bool isPrime = true;
+    for (int i = 2; i < num ; i++)
+        if (num % i == 0) {
+            isPrime = false;
+            break;
+        }
+    return isPrime && num != 1;
+}
+
 int main() {
     int number = 1;
-    int leftToRightMax, rightToLeftMax;
-    const int rowCount = 5;
-    const int columnCount = 5;
+    const int rowCount = 6;
+    const int columnCount = 6;
     int tablica[rowCount][columnCount];
 
-    for (int i = 0; i < rowCount; i++)
-        for (int j = 0; j < columnCount; j++, number++) 
+    for (int i = 0; i < rowCount; i++){
+        for (int j = 0; j < columnCount; j++, number++){
              tablica[i][j] = number;
-
-    leftToRightMax = tablica[0][0];
-    rightToLeftMax = tablica[0][columnCount - 1];
-
-    for (int i = 0; i < rowCount; i++) {
-        if (leftToRightMax < tablica[i][i])
-            leftToRightMax = tablica[i][i];
-
-        if (rightToLeftMax < tablica[i][columnCount - 1 - i])
-            rightToLeftMax = tablica[i][columnCount - 1 - i];
-    }
-
-    for (int i = 0; i < rowCount; i++) {
-        for (int j = 0; j < columnCount; j++)
-            std::cout << tablica[i][j] << " ";
+             std::cout << tablica[i][j] << " ";
+        }
         std::cout << std::endl;
     }
 
-    std::cout << "Max na przekatnej od lewej gory do prawego dolu: " << leftToRightMax<<"\n";
-    std::cout << "Max na przekatnej od prawej gory do lewego dolu: " << rightToLeftMax;
+    std::cout << std::endl;
+
+    for (int i = 0; i < rowCount; i++)
+        for (int j = 0; j < columnCount; j++)
+            if (isPrime(tablica[i][j]))
+                std::cout << tablica[i][j] << " ";
     return 0;
 }
