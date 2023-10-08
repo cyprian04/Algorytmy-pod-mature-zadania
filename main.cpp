51755f234a67d09413e173f1de4dd550d6874bae
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-08 19:33:36 +0200

    zad 7

--- main.cpp
+++ main.cpp
@@ -2,18 +2,24 @@
 
 int main() {
     int number = 1;
+    int leftToRightMax, rightToLeftMax;
     const int rowCount = 5;
     const int columnCount = 5;
-    int sums[columnCount] = {0};
     int tablica[rowCount][columnCount];
 
     for (int i = 0; i < rowCount; i++)
         for (int j = 0; j < columnCount; j++, number++) 
              tablica[i][j] = number;
 
+    leftToRightMax = tablica[0][0];
+    rightToLeftMax = tablica[0][columnCount - 1];
+
     for (int i = 0; i < rowCount; i++) {
-        for (int j = 0; j < columnCount; j++)
-            sums[j] += tablica[i][j];
+        if (leftToRightMax < tablica[i][i])
+            leftToRightMax = tablica[i][i];
+
+        if (rightToLeftMax < tablica[i][columnCount - 1 - i])
+            rightToLeftMax = tablica[i][columnCount - 1 - i];
     }
 
     for (int i = 0; i < rowCount; i++) {
@@ -22,9 +28,7 @@ int main() {
         std::cout << std::endl;
     }
 
-    std::cout << std::endl;
-    for (auto s: sums)
-        std::cout << s <<" ";
-
+    std::cout << "Max na przekatnej od lewej gory do prawego dolu: " << leftToRightMax<<"\n";
+    std::cout << "Max na przekatnej od prawej gory do lewego dolu: " << rightToLeftMax;
     return 0;
 }
