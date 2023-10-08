5be2545899048b7849f0f51fdc7c27b4cdd064d0
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-10-08 15:25:22 +0200

    zad 6

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main() {
     int number = 1;
     const int rowCount = 5;
     const int columnCount = 5;
+    int sums[columnCount] = {0};
     int tablica[rowCount][columnCount];
 
     for (int i = 0; i < rowCount; i++)
@@ -11,11 +12,19 @@ int main() {
              tablica[i][j] = number;
 
     for (int i = 0; i < rowCount; i++) {
-        int sum = 0;
         for (int j = 0; j < columnCount; j++)
-             sum += tablica[i][j];
-        std::cout << sum <<" ";
+            sums[j] += tablica[i][j];
     }
+
+    for (int i = 0; i < rowCount; i++) {
+        for (int j = 0; j < columnCount; j++)
+            std::cout << tablica[i][j] << " ";
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+    for (auto s: sums)
+        std::cout << s <<" ";
 
     return 0;
 }
