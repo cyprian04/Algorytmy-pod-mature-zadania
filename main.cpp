b46bd70e325d46b73885508274f0662f1bf5e96d
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-09 09:19:09 +0200

    zad 14

--- main.cpp
+++ main.cpp
@@ -1,37 +1,19 @@
 ﻿#include <iostream>
-/*
-*   10 11    10 12 14 17 18
-*   12 13    11 13 15 17 19
-*   14 15 => 
-*   16 17
-*   18 19
-
-*/
 
 int main() {
-    char number = 10;
     const int rowCount = 5;
-    const int columnCount = 2;
+    const int columnCount = 5;
     int tablica[rowCount][columnCount];
-    int pomocnicza[columnCount][rowCount];
+    int nums[rowCount] = {1, 4, 9, 16, 25};
 
     for (int i = 0; i < rowCount; i++){
-        for (int j = 0; j < columnCount; j++, number++){
-             tablica[i][j] = number;
-             std::cout << tablica[i][j] << " ";
+        for (int j = 0; j < columnCount; j++){
+            if (i == j)
+                tablica[i][j] = nums[i];
+            else
+                tablica[i][j] = 0;
+            std::cout << tablica[i][j] << " ";
         }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-
-    for (int i = 0; i < columnCount; i++)
-        for (int j = 0; j < rowCount; j++)
-            pomocnicza[i][j] = tablica[j][i];
-             
-
-    for (int i = 0; i < columnCount; i++) {
-        for (int j = 0; j < rowCount; j++)
-            std::cout << pomocnicza[i][j] << " ";
         std::cout << std::endl;
     }
 
