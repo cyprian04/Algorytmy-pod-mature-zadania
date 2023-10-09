3854a9f6a72555294a04c1389ab39065cfb8e43c
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-09 09:35:12 +0200

    zad 15

--- main.cpp
+++ main.cpp
@@ -3,19 +3,17 @@
 int main() {
     const int rowCount = 5;
     const int columnCount = 5;
-    int tablica[rowCount][columnCount];
-    int nums[rowCount] = {1, 4, 9, 16, 25};
+    int tablica[rowCount][columnCount] = {0}; // jeśli mogłem tak zastosować bo w tym podpunkcie nie było nic o pętli
+    int nums[rowCount] = {5, 10, 20, 40, 80};
+
+    for (int i = 0; i < rowCount; i++) 
+         tablica[i][columnCount -1 -i] = nums[i];
 
     for (int i = 0; i < rowCount; i++){
-        for (int j = 0; j < columnCount; j++){
-            if (i == j)
-                tablica[i][j] = nums[i];
-            else
-                tablica[i][j] = 0;
+        for (int j = 0; j < columnCount; j++) {
             std::cout << tablica[i][j] << " ";
         }
         std::cout << std::endl;
     }
-
     return 0;
 }
