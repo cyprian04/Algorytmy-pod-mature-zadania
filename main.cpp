b5cbfd8cd4c44589fa9f768a9a87c1620ad9b833
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-11 19:09:33 +0200

    poprawione 6

--- main.cpp
+++ main.cpp
@@ -1,16 +1,30 @@
 ﻿#include <iostream>
 
-long long countTheFactorial(int num) {
-    if (num == 1 || num == 0)
-        return 1;
-    return countTheFactorial(num - 1) * num;
+bool checkPesel(const std::string pesel_in) {
+    int weight[11] = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1 };
+    int sum = 0;
+    int change = 0;
+
+    for (int i = 0; i < 11; i++)
+        sum += (pesel_in[i] - 48) * weight[i];
+    change = sum % 10;
+
+    return change == 0;
 }
 
 int main() {
-    int factorial = 0;
-    std::cout << "Podaj silnie do wyliczenia: ";
-    std::cin >> factorial;
-    std::cout << factorial << "! wynosi: " << countTheFactorial(factorial);
+    std::string pesel;
+    pesel.resize(11);
+
+    for (size_t i = 0; i < pesel.length(); i++) {
+        std::cout << "Podaj " << i + 1 << " cyfre peselu: ";
+        std::cin >> pesel[i];
+    }
+
+    if (checkPesel(pesel))
+        std::cout << "Pesel jest poprawny :)";
+    else
+        std::cout << "Pesel nie jest poprawny :(";
 
     return 0;
 }
