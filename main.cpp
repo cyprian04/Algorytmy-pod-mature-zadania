20940a6a79740ac79cc8eee00232a4e28ffd2d52
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-11 19:03:20 +0200

    poprawione 11

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 ﻿#include <iostream>
 
-long long CountTheFactorial(int num) {
+long long countTheFactorial(int num) {
     if (num == 1 || num == 0)
-        return num;
-    return CountTheFactorial(num - 1) * num;
+        return 1;
+    return countTheFactorial(num - 1) * num;
 }
 
 int main() {
     int factorial = 0;
     std::cout << "Podaj silnie do wyliczenia: ";
     std::cin >> factorial;
-    std::cout << factorial << "! wynosi: " << CountTheFactorial(factorial);
+    std::cout << factorial << "! wynosi: " << countTheFactorial(factorial);
 
     return 0;
 }
