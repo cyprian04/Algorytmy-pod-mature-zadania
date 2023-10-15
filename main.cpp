c251e3b637056aa1326cc811064ddce3dbfd668f
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-10-15 13:59:31 +0200

    zad 10

--- main.cpp
+++ main.cpp
@@ -2,21 +2,33 @@
 #include <fstream>
 using namespace std;
 
+bool haveOnlyOneSign(const string text, const char sign) { 
+    for (size_t i = 0; i < text.length(); i++)
+        if (text[i] != sign)
+            return false;
+    return true;
+}
+
 int main() {
     string text;
     fstream file;
     ofstream saveFile;
     file.open("napisy.txt");
     saveFile.open("zadanie4.txt");
-    int counter = 0;
+    int counterZero = 0;
+    int counterOne = 0;
 
     while (!file.eof()) {
         file >> text;
-        if (text.length() % 2 == 0)
-            counter++;
-        
+        if (haveOnlyOneSign(text, '0'))
+            counterZero++;
+        else if (haveOnlyOneSign(text, '1'))
+            counterOne++;
     }
-    saveFile << "a) " << counter << endl;
+
+    saveFile << "c)\n" << "Liczba napisów z samych 0: "<<counterZero << endl;
+    saveFile << "Liczba napisów z samych 1: " << counterOne << endl;
+
 
     file.close();
     saveFile.close();
