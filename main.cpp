fbd8f99e8a0b119667661735eb7b3c88aeb54b6c
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-10-15 11:44:15 +0200

    zad 7

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 #include <fstream>
 using namespace std;
 
-int sumOfDigits(const string text) {
-    int sum = 0;
-    for (size_t i = 0; i < text.length(); i++)
-        sum += (text[i] - 48);
-    return sum;
+bool isIncreasing(const string text) {
+    for (size_t i = 0; i < text.length() - 1; i++)
+        if (text[i] >= text[i + 1])
+            return false;
+
+    return true;
 }
 
 int main() {
@@ -15,27 +16,14 @@ int main() {
     ofstream saveFile;
     file.open("cyfry.txt");
     saveFile.open("zadanie4.txt");
-    int maxNumCount = 0;
-    int minNumCount = 81; // max możliwość
-    string minNum, maxNum;
+
+    saveFile << "c) " << endl;
 
     while (!file.eof()) {
         file >> text;
-
-        if (sumOfDigits(text) > maxNumCount) {
-            maxNumCount = sumOfDigits(text);
-            maxNum = text;
-        }
-        if (sumOfDigits(text) < minNumCount) {
-            minNumCount = sumOfDigits(text);
-            minNum = text;
-        }
+        if (isIncreasing(text))
+            saveFile << text << endl;
     }
-
-    saveFile << "b) "<< endl;
-    saveFile << "Największa suma cyfr jest w wartości: "<< maxNum <<endl;
-    saveFile << "Najmniejsza suma cyfr jest w wartości: " << minNum<<endl;
-
 
     file.close();
     saveFile.close();
