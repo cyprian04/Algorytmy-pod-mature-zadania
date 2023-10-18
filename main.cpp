a6807c5ce4ce3f6ebd4cbb48b5d4c9ee6a821b1e
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-18 20:31:47 +0200

    zad 4

--- main.cpp
+++ main.cpp
@@ -2,30 +2,32 @@
 #include <vector>
 using namespace std;
 
-bool numsAreDivided(vector<int> vec) {
+float arithmeticAverage(vector<float> vec) {
+    float result = 0.0f;
     for (size_t i = 0; i < vec.size(); i++)
-        if (vec[i] % 13 == 0)
-            return false;
-    return true;
+        result += vec[i];
+
+    return result/vec.size();
 }
 
 int main() {
     int size = 0;
-    vector<int> nums;
+    vector<float> nums;
     cout << "Podaj ilosc liczb jaka chcesz wprowadzic :";
     cin >> size;
 
+    if (size == 0) {
+        cout << " Srednia arytmetyczna tego wektora wynosi 0";
+        return 0;
+    }   
+
     for (size_t i = 0; i < size; i++) {
-        int temp = 0;
+        float temp = 0;
         cout << "Podaj liczbe nr " << i + 1 << ": ";
         cin >> temp;
         nums.push_back(temp);
     }
 
-    if (numsAreDivided(nums))
-        cout << "Wszystkie liczby wektora nie sa podzielne przez 13";
-    else 
-        cout << "Co najmniej jedna z liczb wektora jest podzielna przez 13";
-
+    cout << "Srednia arytmetyczna tego wektora wynosi: " << arithmeticAverage(nums);
     return 0;
 }
