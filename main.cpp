e978737dd1ecb792b31a300a41cc0b74d5fcb2ed
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-18 20:03:41 +0200

    zad 2

--- main.cpp
+++ main.cpp
@@ -2,24 +2,24 @@
 #include <vector>
 using namespace std;
 
+int sumOfEvenNums(vector<int> vec) {
+    int sum = 0;
+
+    for (size_t i = 0; i < vec.size(); i++)
+        if (vec[i] % 2 == 0)
+            sum += vec[i];
+    return sum;
+}
+
 int main() {
     int num = 0;
-    vector<int> nums;
-    cout << "Podaj liczbe: ";
-    cin >> num;
+    vector<int> nums = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    if (num == 0) nums.push_back(0);
+    cout << "Wektor posiada liczby: ";
+    for (const auto &n : nums)
+        cout << n <<" ";
 
-    while (num != 0) {
-        int digit = num % 10 ;     
-        num -= digit;
-        num /= 10;
-   
-        nums.push_back(digit);
-    }
-
-    for (size_t i = 0; i < nums.size(); i++)
-        cout << nums[nums.size() - 1 - i];
+    cout << "\nSuma liczb parzystych tego wektora: "<< sumOfEvenNums(nums);
 
     return 0;
 }
