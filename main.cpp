4ca78fcceb9f6eb37cac48cbd755ce1f6e5d39a4
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-18 20:21:02 +0200

    poprawione 3

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-int numsAreDivided(vector<int> vec) {
+bool numsAreDivided(vector<int> vec) {
     for (size_t i = 0; i < vec.size(); i++)
         if (vec[i] % 13 == 0)
             return false;
