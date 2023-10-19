5d71771701a8f64b005377f6a0ff3422d3f14cfb
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-19 11:28:37 +0200

    zad 9

--- main.cpp
+++ main.cpp
@@ -1,20 +1,32 @@
 ﻿#include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int sum(vector<int> vec, int size) {
-    if (size == 0)
-        return vec[0];
-    return vec[size] + sum(vec, size - 1);
+vector<int> filteredVec(vector<int> &vec) {
+    vector<int> nums;
+    int maxNum = *max_element(vec.begin(), vec.end());
+    int minNum = *min_element(vec.begin(), vec.end());
+    int treshold = (maxNum + minNum) / 2;
+
+    for (size_t i = 0; i < vec.size(); i++)
+        if (vec[i] >= treshold)
+            nums.push_back(vec[i]); 
+
+    return nums;
 }
 
 int main() {
-    vector<int> nums = { 1,2,3,4 };
+    vector<int> nums = { 1,2,3,4,5,6,7,8,9 };
 
     cout << "mamy wektor\n";
     for (const auto n : nums)
-        cout << n << ", ";
+         cout << n << ", ";
 
-    cout << "Suma elementow wektora wynosi: " << sum(nums, nums.size() - 1);
+    vector<int>filtered = filteredVec(nums);
+
+    cout << "\nZaktualizowany wektor: \n";
+    for (const auto n : filtered)
+        cout << n << ", ";
     return 0;
 }
