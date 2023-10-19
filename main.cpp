69131d366b25a3ca1f1f285ac3ecfe1938a911dc
Cyprian <Cyprian.Gibas@o2.pl>
2023-10-19 10:35:41 +0200

    zad 7

--- main.cpp
+++ main.cpp
@@ -2,32 +2,41 @@
 #include <vector>
 using namespace std;
 
-bool isPalindrom(const vector<int> vec) {
-    for (size_t i = 0; i < vec.size(); i++)
-        if (vec[i] != vec[vec.size() - 1 - i])
+bool isPrime(int num) {
+    for (int i = 2; i < num; i++)
+        if (num % i == 0)
             return false;
-    return true;
+    return (true && num != 1);
+}
+
+void swapThePrimeValues(vector<int> &vec1, vector<int> &vec2) {
+    for (size_t i = 0; i < vec1.size(); i++)
+        if (isPrime(vec1[i]))
+            swap(vec1[i], vec2[i]);
 }
 
 int main() {
-    int size = 0;
-    vector<int> nums;
-    cout << "Podaj ile liczb chcesz wprowadzic :";
-    cin >> size;
+    vector<int> nums1 = { 1,2,17,4 };
+    vector<int> nums2 = { 5,6,7,8 };
+    cout << "mamy 2 wektory";
 
-    if (size == 0) return 0;
+    cout << "\nwektor 1\n";
+    for (const auto n : nums1)
+        cout << n << ", ";
 
-    for (size_t i = 0; i < size; i++) {
-        int temp = 0;
-        cout << "Podaj napis nr " << i + 1 << ": ";
-        cin >> temp;
-        nums.push_back(temp);
-    }
+    cout << "\nwektor 2\n";
+    for (const auto n : nums2)
+        cout << n << ", ";
 
-    if (isPalindrom(nums))
-        cout << "wektor jest palindromem";
-    else
-        cout << "wektor nie jest palindromem";
+    cout << " \n\nPo zamienieniu w nich elementow miejscami, ktorych wartosci sa liczbami pierwszymi z wektora podanego jako pierwszy argument\n";
+    swapThePrimeValues(nums1, nums2);
 
+    cout << "\nwektor 1\n";
+    for (const auto n : nums1)
+        cout << n << ", ";
+
+    cout << "\nwektor 2\n";
+    for (const auto n : nums2)
+        cout << n << ", ";
     return 0;
 }
