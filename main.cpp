264dbb196611548f853946987e9ae1021feb082d
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-10-24 09:15:03 +0200

    poprawione zadnie 6

--- main.cpp
+++ main.cpp
@@ -2,35 +2,57 @@
 #include <vector>
 using namespace std;
 
-int silnia(int num) {
-    int sum = 1;
-    for (int i = 1; i <= num; i++)
-        sum *= i;
-    return sum;
-}
+string splitNumToDigit(int num) {
+    string result;
+    string temp;
+    int change = 0;
 
-vector<int> silniaN(int n) {
-    vector<int> result;
-    for (int i = 1; i <=n; i++) {
-        int temp = silnia(i);
-        result.push_back(temp);
+    while (num > 0) {
+        change = num % 10;
+        temp = result;
+        result = change + 48;
+        result += temp;
+
+        num -= change;
+        num /= 10;
     }
     return result;
 }
 
-int main() {
-    int n = 0;
-    cout << "Podaj silnie: ";
-    cin >> n;
+bool isPalindrom(const vector<int> vec) {
+    string filteredVec;
 
-    if (n == 0) {
-        cout << "1";
-        return 0;
+    for (size_t i = 0; i < vec.size(); i++)
+        if (vec[i] <= 9)
+            filteredVec += vec[i] + 48;
+        else
+            filteredVec += splitNumToDigit(vec[i]);
+
+    for (size_t i = 0; i < filteredVec.size(); i++)
+        if(filteredVec[i] != filteredVec[filteredVec.size() - 1 - i])
+            return false;
+    return true;
+}
+
+int main() {
+    int size = 0;
+    vector<int> nums;
+    cout << "Podaj ile liczb chcesz wprowadzic :";
+    cin >> size;
+
+    if (size == 0) return 0;
+
+    for (size_t i = 0; i < size; i++) {
+        int temp = 0;
+        cout << "Podaj liczbe nr " << i + 1 << ": ";
+        cin >> temp;
+        nums.push_back(temp);
     }
 
-    vector<int> silnia = silniaN(n);
+    if (isPalindrom(nums))
+        cout << "wektor jest palindromem";
+    else
+        cout << "wektor nie jest palindromem";
 
-    for (auto v : silnia)
-        cout << v << " ";
     return 0;
 }
