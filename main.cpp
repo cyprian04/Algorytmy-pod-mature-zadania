de2a3cff7b7e319b230425d0932cbf54cde56411
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-10-29 17:12:04 +0100

    zad 10

--- main.cpp
+++ main.cpp
@@ -1,26 +1,21 @@
 ﻿#include <iostream>
+#include <cmath>
 using namespace std;
 
-bool isPatternNested(string napis, string wzor) {
-    int counter = 0;
-
-    for (size_t i = 0; i < napis.length(); i++){
-        if (napis[i] == wzor[counter])
-            counter++;
-        else {
-            i -= counter;
-            counter = 0;
-        }
-        if (counter == wzor.length())
-            return true;
-    }
-    return false;
+int polynomial(int nums[],  int size, int degree, int x) {
+    int result = 0;
+    for (int i = 0; i < size; i++)
+       result += nums[i] * pow(x, degree-i);
+    
+    return result;
 }
 
 int main() {
-    if (isPatternNested("AdaaadmAdddAdam", "Adam"))
-        cout << "ciag Adam zawiera sie w napisie AdaaadmAdddAdam";
-    else 
-        cout << "ciag Adam Nie zawiera sie w napisie AdaaadmAdddAdam";
+    const int size = 4;
+    int nums[size] = { 2,1,3,9 };
+    int degree = 3;
+    int x = 10;
+
+    cout << "Wartosc wielomianu: " << polynomial(nums, size, degree, x);
     return 0;
 }
