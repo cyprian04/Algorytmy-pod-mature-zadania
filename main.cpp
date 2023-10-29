f45b7dd85fe8849bb8ceb19d1463f83fc94a5d13
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-10-29 17:19:42 +0100

    zad 11

--- main.cpp
+++ main.cpp
@@ -2,20 +2,17 @@
 #include <cmath>
 using namespace std;
 
-int polynomial(int nums[],  int size, int degree, int x) {
-    int result = 0;
-    for (int i = 0; i < size; i++)
-       result += nums[i] * pow(x, degree-i);
-    
-    return result;
+int polynomial(int nums[], int degree, int x) {
+    if (degree == 0)
+        return nums[0];
+    return x * polynomial(nums, degree - 1, x) + nums[degree];
 }
 
 int main() {
-    const int size = 4;
-    int nums[size] = { 2,1,3,9 };
+    int nums[4] = { 2,1,3,9 };
     int degree = 3;
     int x = 10;
 
-    cout << "Wartosc wielomianu: " << polynomial(nums, size, degree, x);
+    cout << "Wartosc wielomianu: " << polynomial(nums, degree, x);
     return 0;
 }
