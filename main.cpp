fada6920593bb80cd2656fad09b7f76f3029b1d9
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-10-29 12:07:00 +0100

    zad 4

--- main.cpp
+++ main.cpp
@@ -8,6 +8,10 @@ int nwd(int a, int b) {
     return nwd(b, reszta);
 }
 
+int nww(int a, int b) {
+    return a * b / nwd(a, b);
+}
+
 int main() {
 
     int num1 = 0, num2 = 0;
@@ -16,6 +20,6 @@ int main() {
     cin >> num1;
     cout << "Podaj liczbe b: ";
     cin >> num2;
-    cout << "NWD liczby " << num1 << " i liczby " << num2 << " wynosi: " << nwd(num1, num2);
+    cout << "NWW liczby " << num1 << " i liczby " << num2 << " wynosi: " << nww(num1, num2);
     return 0;
 }
