f2102a988dcf510e7df08dfd774521960b1fb122
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-10-31 10:57:10 +0100

    zad 2

--- main.cpp
+++ main.cpp
@@ -1,21 +1,21 @@
 ﻿#include <iostream>
 using namespace std;
 
-void bubbleSortDesc(int tab[], int size) {
+void bubbleSortAsc(int tab[], int size) {
     int n = size - 1;
 
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
-            if (tab[j] < tab[j + 1])
+            if (tab[j] > tab[j + 1])
                 swap(tab[j], tab[j + 1]);
 }
 
 int main() {
     const int size = 5;
     int tab[size] = {8,20,10,15,2};
-    bubbleSortDesc(tab, size);
+    bubbleSortAsc(tab, size);
 
-    cout << "Posortowanie malejaco \n";
+    cout << "Posortowanie rosnaco \n";
     for (auto element : tab)
         cout <<element << " ";
 
