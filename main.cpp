bd8cc65ea9fb7bde013bb0f9245497ec37ed1239
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-10-31 11:14:33 +0100

    zad 3

--- main.cpp
+++ main.cpp
@@ -1,23 +1,47 @@
 ﻿#include <iostream>
 using namespace std;
 
-void bubbleSortAsc(int tab[], int size) {
-    int n = size - 1;
+void toBigger(string& text) {
+    for (size_t i = 0; i < text.length(); i++)
+        if (text[i] >= 'a' && text[i] <= 'z')
+            text[i] -= 32;
+}
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            if (tab[j] > tab[j + 1])
-                swap(tab[j], tab[j + 1]);
+void bubbleSortAsc(string& napis, size_t size) {
+    size_t n = size - 1;
+
+    for (size_t i = 0; i < n; i++)
+        for (size_t j = 0; j < n; j++)
+            if (napis[j] > napis[j + 1])
+                swap(napis[j], napis[j + 1]);
+}
+
+bool isAnagram(string napis1, string napis2) {
+    if (napis1.length() != napis2.length())
+        return false;
+
+    toBigger(napis1);
+    toBigger(napis2);
+    bubbleSortAsc(napis1, napis1.length());
+    bubbleSortAsc(napis2, napis2.length());
+
+    for (size_t i = 0; i < napis1.length(); i++)
+        if (napis1[i] != napis2[i])
+            return false;
+
+    return true;
 }
 
 int main() {
-    const int size = 5;
-    int tab[size] = {8,20,10,15,2};
-    bubbleSortAsc(tab, size);
+    string napis1, napis2;
+    cout << "Podaj napis 1: ";
+    cin >> napis1;
+    cout << "Podaj napis 2: ";
+    cin >> napis2;
 
-    cout << "Posortowanie rosnaco \n";
-    for (auto element : tab)
-        cout <<element << " ";
-
+    if (isAnagram(napis1, napis2))
+        cout << "wyrazy sa anagramami";
+    else
+        cout << "wyrazy nie sa anagramami";
     return 0;
 }
