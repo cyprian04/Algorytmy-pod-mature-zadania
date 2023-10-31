3438d9c99ce7823bb4c21a5efb825d5a89f2def4
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-10-31 17:43:35 +0100

    zad 9

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ void bucketSort(int tab[], int size) {
     for (int i = 0; i < size; i++)
             pomocniczaTab[tab[i]]++;
      
-    for (int i = 0; i < maxElement+1; i++) {
+    for (int i = maxElement+1; i > 0 ; i--) {
         if(pomocniczaTab[i] != 0)
             for (int j = 0; j < pomocniczaTab[i]; j++)
                 cout << i << " ";
