253bee9f4445d03897d9cd8875f564a8921ec05b
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-10-31 13:19:06 +0100

    zad 5

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ void selectionSort(int tab[], int size) {
     for (int j = 0; j < size - 1; j++) {
         int index = j;
         for (int i = j + 1; i < size; i++) {
-            if (tab[i] > tab[index])
+            if (tab[i] < tab[index])
                 index = i;
         }
         swap(tab[j], tab[index]);
