1df69006ba6b6829d9041b7b6fff12896c88aecf
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-11-01 09:41:36 +0100

    zad 9

--- main.cpp
+++ main.cpp
@@ -1,43 +1,26 @@
 ﻿#include <iostream>
+#include <algorithm>
 using namespace std;
 
-void toBigger(string& text) {
-    for (size_t i = 0; i < text.length(); i++)
-        if (text[i] >= 'a' && text[i] <= 'z')
-            text[i] -= 32;
-}
+void bucketSort(int tab[], int size) {
+    int maxElement = *max_element(tab, tab + size);
+    int* pomocniczaTab = new int[++maxElement]{ 0 };
 
-void bubbleSortAsc(string& napis, size_t size) {
-    size_t n = size - 1;
+    for (int i = 0; i < size; i++)
+        pomocniczaTab[tab[i]]++;
 
-    for (size_t i = 0; i < n; i++)
-        for (size_t j = 0; j < n; j++)
-            if (napis[j] > napis[j + 1])
-                swap(napis[j], napis[j + 1]);
-}
-
-bool isAnagram(string napis1, string napis2) {
-    if (napis1.length() != napis2.length())
-        return false;
-
-    toBigger(napis1);
-    toBigger(napis2);
-    bubbleSortAsc(napis1, napis1.length());
-    bubbleSortAsc(napis2, napis2.length());
-
-    return napis1 == napis2;
+    for (int i = maxElement; i >= 0; i--) {
+        if (pomocniczaTab[i] != 0)
+            for (int j = 0; j < pomocniczaTab[i]; j++)
+                cout << i << " ";
+    }
+    delete[] pomocniczaTab;
 }
 
 int main() {
-    string napis1, napis2;
-    cout << "Podaj napis 1: ";
-    cin >> napis1;
-    cout << "Podaj napis 2: ";
-    cin >> napis2;
+    const int size = 8;
+    int tab[size] = { 8,20,10,15,2,2,10,1 };
+    bucketSort(tab, size);
 
-    if (isAnagram(napis1, napis2))
-        cout << "wyrazy sa anagramami";
-    else
-        cout << "wyrazy nie sa anagramami";
     return 0;
 }
