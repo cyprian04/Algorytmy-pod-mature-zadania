174cb147ed2b1653d29bfecf0e79f14fd645c287
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-11-01 11:48:45 +0100

    zad 11

--- main.cpp
+++ main.cpp
@@ -3,39 +3,10 @@
 #include <vector>
 using namespace std;
 
-string toBigger(string text) {
-    for (size_t i = 0; i < text.length(); i++)
-        if (text[i] >= 'a' && text[i] <= 'z')
-            text[i] -= 32;
-    return text;
-}
-
-string selectionSort(string napis, size_t size) {
-        for (size_t i = 0; i < size -1; i++) {
-            size_t index = i;
-            for (size_t j = i + 1; j < size; j++) {
-                if (napis[index] > napis[j])
-                    index = j;
-            }
-            swap(napis[index], napis[i]);
-        }
-    return napis;
-}
-
-bool isAnagram(vector<string> strings) {
-
+bool areSameLength(vector<string> strings) {
     for (int i = 0; i < 5; i++)
         for (int j = 0; j < 5; j++)
             if (strings[i].length() != strings[j].length())
-                return false;
-
-    vector<string> filtered(5);
-    for (size_t i = 0; i < 5; i++)
-        filtered[i] = selectionSort(toBigger(strings[i]), strings[i].length());
-
-    for (int i = 0; i < 5; i++)
-        for (int j = 0; j < 5; j++)
-            if (filtered[i] != filtered[j])
                 return false;
    return true;
 }
@@ -44,16 +15,16 @@ int main() {
     fstream file;
     ofstream saveFile;
     file.open("anagram.txt");
-    saveFile.open("odp_4b.txt");
+    saveFile.open("odp_4a.txt");
 
-    saveFile << "b)\n";
+    saveFile << "a)\n";
 
     while (!file.eof()) {
         vector<string> strings(5);
         for (int i = 0; i < 5; i++)
             file >> strings[i];
 
-        if (isAnagram(strings)) {
+        if (areSameLength(strings)) {
             for (int i = 0; i < 5; i++)
                 saveFile << strings[i] << " ";
             saveFile << endl;
