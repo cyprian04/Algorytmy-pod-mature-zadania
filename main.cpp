212a39031fdff51fc154513e88ae69f840eb6901
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-11-01 10:18:56 +0100

    zad 9 poprawione

--- main.cpp
+++ main.cpp
@@ -4,24 +4,22 @@ using namespace std;
 
 void bucketSort(int tab[], int size) {
     int maxElement = *max_element(tab, tab + size);
-    int minElement = *min_element(tab, tab + size);
-    int newSize = maxElement + 1 - minElement;
-    int* pomocniczaTab = new int[newSize]{ 0 };
+    int* pomocniczaTab = new int[maxElement +1]{ 0 };
 
     for (int i = 0; i < size; i++)
-        pomocniczaTab[tab[i]-minElement]++;
+        pomocniczaTab[tab[i]]++;
 
-    for (int i = newSize; i >= 0; i--) {
+    for (int i = maxElement; i >= 0; i--) {
         if (pomocniczaTab[i] != 0)
             for (int j = 0; j < pomocniczaTab[i]; j++)
-                cout << i + minElement << " ";
+                cout << i << " ";
     }
     delete[] pomocniczaTab;
 }
 
 int main() {
     const int size = 8;
-    int tab[size] = { 8,20,10,15,-2,2,-10,1 };
+    int tab[size] = { 8,20,10,15,2,2,10,1 };
     bucketSort(tab, size);
 
     return 0;
