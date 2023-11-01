a2cada25b5e8775f0d8e3f120a444ef4f5d609f0
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-11-01 11:42:46 +0100

    zad 12

--- main.cpp
+++ main.cpp
@@ -1,26 +1,65 @@
 ﻿#include <iostream>
-#include <algorithm>
+#include <fstream>
+#include <vector>
 using namespace std;
 
-void bucketSort(int tab[], int size) {
-    int maxElement = *max_element(tab, tab + size);
-    int* pomocniczaTab = new int[maxElement + 1]{ 0 };
+string toBigger(string text) {
+    for (size_t i = 0; i < text.length(); i++)
+        if (text[i] >= 'a' && text[i] <= 'z')
+            text[i] -= 32;
+    return text;
+}
 
-    for (int i = 0; i < size; i++)
-        pomocniczaTab[tab[i]]++;
+string selectionSort(string napis, size_t size) {
+        for (size_t i = 0; i < size -1; i++) {
+            size_t index = i;
+            for (size_t j = i + 1; j < size; j++) {
+                if (napis[index] > napis[j])
+                    index = j;
+            }
+            swap(napis[index], napis[i]);
+        }
+    return napis;
+}
 
-    for (int i = 0; i < maxElement + 1; i++) {
-        if (pomocniczaTab[i] != 0)
-            for (int j = 0; j < pomocniczaTab[i]; j++)
-                cout << i << " ";
-    }
-    delete[] pomocniczaTab;
+bool isAnagram(vector<string> strings) {
+
+    for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 5; j++)
+            if (strings[i].length() != strings[j].length())
+                return false;
+
+    vector<string> filtered(5);
+    for (size_t i = 0; i < 5; i++)
+        filtered[i] = selectionSort(toBigger(strings[i]), strings[i].length());
+
+    for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 5; j++)
+            if (filtered[i] != filtered[j])
+                return false;
+   return true;
 }
 
 int main() {
-    const int size = 8;
-    int tab[size] = { 8,20,10,15,2,2,10,1 };
-    bucketSort(tab, size);
+    fstream file;
+    ofstream saveFile;
+    file.open("anagram.txt");
+    saveFile.open("odp_4b.txt");
 
+    saveFile << "b)\n";
+
+    while (!file.eof()) {
+        vector<string> strings(5);
+        for (int i = 0; i < 5; i++)
+            file >> strings[i];
+
+        if (isAnagram(strings)) {
+            for (int i = 0; i < 5; i++)
+                saveFile << strings[i] << " ";
+            saveFile << endl;
+        }
+    }
+    file.close();
+    saveFile.close();
     return 0;
 }
