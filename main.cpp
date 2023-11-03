421e92aeb7bac326100ca0cc9b366d271c04e614
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-11-03 13:00:35 +0100

    zad 5

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 ﻿#include <iostream>
 using namespace std;
 
-void merge(int tab[], int lewyIndex, int pivot, int prawyIndex, int pomocnicza[]) {
+void merge(int tab[3][4], int column, int lewyIndex, int pivot, int prawyIndex, int pomocnicza[3][4]) {
 	for (int i = lewyIndex; i <= prawyIndex; i++)
-		pomocnicza[i] = tab[i];
+		pomocnicza[i][column] = tab[i][column];
 
 	int indexLewej = lewyIndex;
 	int indexPrawej = pivot + 1;
@@ -11,38 +11,38 @@ void merge(int tab[], int lewyIndex, int pivot, int prawyIndex, int pomocnicza[]
 
 	while (indexLewej <= pivot && indexPrawej <= prawyIndex) {
 
-		if (pomocnicza[indexLewej] <= pomocnicza[indexPrawej]) {
-			tab[index] = pomocnicza[indexLewej];
+		if (pomocnicza[indexLewej][column] <= pomocnicza[indexPrawej][column]) {
+			tab[index][column] = pomocnicza[indexLewej][column];
 			indexLewej++;
 		}
 		else {
-			tab[index] = pomocnicza[indexPrawej];
+			tab[index][column] = pomocnicza[indexPrawej][column];
 			indexPrawej++;
 		}
 		index++;
 	}
 
 	while (indexLewej <= pivot) {
-		tab[index] = pomocnicza[indexLewej];
+		tab[index][column] = pomocnicza[indexLewej][column];
 		indexLewej++;
 		index++;
 	}
 }
 
-void mergeSort(int tab[], int lewyIndex, int prawyIndex, int pomocnicza[]) {
+void mergeSort(int tab[3][4], int column ,int lewyIndex, int prawyIndex, int pomocnicza[3][4]) {
 
 	if (lewyIndex != prawyIndex) {
 		int pivot = (lewyIndex + prawyIndex) / 2;
 
-		mergeSort(tab, lewyIndex, pivot, pomocnicza);
-		mergeSort(tab, pivot+1, prawyIndex, pomocnicza);
-		merge(tab, lewyIndex, pivot, prawyIndex, pomocnicza);
+		mergeSort(tab, column, lewyIndex, pivot, pomocnicza);
+		mergeSort(tab, column, pivot+1, prawyIndex, pomocnicza);
+		merge(tab, column, lewyIndex, pivot, prawyIndex, pomocnicza);
 	}
 }
 
 int main() {
 	int tab[3][4] = { {4,5,6,4}, {3,3,2,7}, {1,1,1,1} };
-	int pomocnicza[4];
+	int pomocnicza[3][4];
 
 	cout << "Przed sortowaniem \n";
 	for (int i = 0; i < 3; i++) {
@@ -51,10 +51,10 @@ int main() {
 	  cout << endl;
 	}
 	
-	for (int i = 0; i < 3; i++) 
-		mergeSort(tab[i], 0, 3, pomocnicza); // przekazuje tab[i] jako wskaźnik do danego wierza 2 wymiarowej tablicy. przez co przekazuje 1 wymiarową do funkcji
+	for (int i = 0; i < 4; i++) 
+		mergeSort(tab, i, 0, 2, pomocnicza); 
 
-	cout << "\n Po sortowaniu \n";
+	cout << "\nPo sortowaniu \n";
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 4; j++)
 			cout << tab[i][j] << " ";
