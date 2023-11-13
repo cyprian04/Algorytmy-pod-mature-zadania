f975997961e3aba16a728c9c65f30f1d01551dcb
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-13 19:48:43 +0100

    zad 5

--- main.cpp
+++ main.cpp
@@ -6,12 +6,19 @@ void encryption(string& text) {
 		swap(text[i], text[i + 1]);
 }
 
+void decryption(string& text) {
+	for (size_t i = 0; i < text.length() - 1; i += 2)
+		swap(text[i], text[i + 1]);
+}
+
 int main() {
 	string text;
 	cout << "Podaj napis: ";
 	cin >> text;
 	encryption(text);
 	cout << "encryptet to: " << text;
+	decryption(text);
+	cout << "\ndecryptet to: " << text;
 
 	return 0;
 }
