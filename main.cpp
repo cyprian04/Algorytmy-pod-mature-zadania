bb57792ab29cdb2e91e6c67d99d798236aa146b0
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-13 19:37:27 +0100

    zad 3

--- main.cpp
+++ main.cpp
@@ -10,6 +10,15 @@ void encryption(int key, string& text) {
 			text[i] += key - 26;
 }
 
+void decryption(int key, string& text) {
+	key %= 26;
+	for (size_t i = 0; i < text.length(); i++)
+		if (text[i] - key >= 'A')
+			text[i] -= key;
+		else
+			text[i] += -key + 26;
+}
+
 int main() {
 	string tekst;
 	int key = 0;
@@ -21,5 +30,7 @@ int main() {
 
 	encryption(key, tekst);
 	cout <<"encryptet to: "<< tekst;
+	decryption(key, tekst);
+	cout << "\ndecryptet to: " << tekst;
 	return 0;
 }
