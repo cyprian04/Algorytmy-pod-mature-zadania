778c4ddc869374e739d6d6ff6915ce4309549e5b
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-13 19:21:30 +0100

    zad 2

--- main.cpp
+++ main.cpp
@@ -1,40 +1,25 @@
 ﻿#include <iostream>
 using namespace std;
 
-void quickSort(int tab[], int lewyIndex, int prawyIndex) {
-
-	if (lewyIndex >= prawyIndex) return;
-
-	int pivotIndex = (lewyIndex + prawyIndex)/2;
-	int pivot = tab[pivotIndex];
-	int index = lewyIndex;
-	int granica = lewyIndex - 1;
-
-	swap(tab[pivotIndex], tab[prawyIndex]);
-
-	while(index < prawyIndex) {
-		if (tab[index] < pivot) {
-			granica++;
-			if(granica != index)
-				swap(tab[granica], tab[index]);
-		}
-		index++;
-	}
-
-	granica++;
-	if (granica != prawyIndex)
-		swap(tab[granica], tab[prawyIndex]);
-
-	quickSort(tab, lewyIndex, granica - 1);
-	quickSort(tab, granica + 1, prawyIndex);
+void encryption(int key, string& text) {
+	key %= 26;
+	for (size_t i = 0; i < text.length(); i++)
+		if (text[i] + key <= 'Z')
+			text[i] += key;
+		else
+			text[i] += key - 26;
 }
 
 int main() {
-	int tab[9] = { 8,1,2,3,7,8,1,2,4 };
+	string tekst;
+	int key = 0;
 
-	quickSort(tab, 0, 8);
+	cout << "Podaj tekst: ";
+	cin >> tekst;
+	cout << "Podaj klucz: ";
+	cin >> key;
 
-	for (auto num : tab)
-		cout << num << " ";
+	encryption(key, tekst);
+	cout <<"encryptet to: "<< tekst;
 	return 0;
 }
