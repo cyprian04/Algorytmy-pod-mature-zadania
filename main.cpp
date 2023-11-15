3f9cb63376e4805145bf74ee771cfe545bbe03f4
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-15 20:54:07 +0100

    zad 8

--- main.cpp
+++ main.cpp
@@ -1,65 +1,40 @@
 ﻿#include <iostream>
 using namespace std;
 
-void encryption(int key, string text) {
-	char** tablica = new char*[key];
-	for (int i = 0; i < key; i++)
-		tablica[i] = new char[text.length()];
-
-	for (int i = 0; i < key; i++)
-		for (int j = 0; j < text.length(); j++)
-			tablica[i][j] = '*';
-
-
-	int pos = 0;
-	bool goDown = true;
-	for (int i = 0; i < text.length(); i++) {
-
-		if (goDown == true && pos != key - 1) {
-			tablica[pos][i] = text[i];
-			pos++;
-		}
-		else if (goDown == true && pos == key - 1) {
-			tablica[pos][i] = text[i];
-			pos--;
-			goDown = false;
-		}
-		else if (goDown == false && pos != 0) {
-			tablica[pos][i] = text[i];
-			pos--;
-		}
-		else if (goDown == false && pos == 0) {
-			tablica[pos][i] = text[i];
-			pos++;
-			goDown = true;
-		}
-	}
-
-	for (int i = 0; i < key; i++)
-		for (int j = 0; j < text.length(); j++)
-			if(tablica[i][j] !='*')
-				cout <<tablica[i][j];
-
-	for (int i = 0; i < key; i++)
-		delete[] tablica[i];
-	delete[] tablica;
+void encryption(int key, string& text) {
+	key %= 26;
+	for (size_t i = 0; i < text.length(); i++)
+		if (key < 0 && (text[i]+ key) < 'A')
+			text[i] += key + 26;
+		else if (text[i] + key <= 'Z')
+			text[i] += key;
+		else
+			text[i] += key - 26;
 }
 
-void decryption(string text) {
-	for (size_t i = 0; i < text.length() - 1; i += 2)
-		swap(text[i], text[i + 1]);
+void decryption(int key, string& text) {
+	key %= 26;
+	for (size_t i = 0; i < text.length(); i++)
+		if(key < 0 && (text[i]- key) > 'Z')
+			text[i] -= key + 26;
+		else if (text[i] - key >= 'A')
+			text[i] -= key;
+		else
+			text[i] += -key + 26;
 }
 
 int main() {
-	string text;
-	int key = 1;
+	string tekst;
+	int key = 0;
 
-	cout << "Podaj napis: ";
-	cin >> text;
+	cout << "Podaj tekst: ";
+	cin >> tekst;
 	cout << "Podaj klucz: ";
 	cin >> key;
-	cout << "encryptet to: ";
-	encryption(key,text);
 
+	encryption(key, tekst);
+	cout << "encryptet to: " << tekst;
+	decryption(key, tekst);
+	cout << "\ndecryptet to: " << tekst;
 	return 0;
 }
