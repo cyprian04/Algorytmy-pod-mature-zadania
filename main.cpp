1c50a6925fc63cb9680e4039a53a2c72b179d2d1
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-11-17 17:44:16 +0100

    zad 11

--- main.cpp
+++ main.cpp
@@ -2,32 +2,32 @@
 #include <fstream>
 using namespace std;
 
-string encryption(const string text, const string key ) {
+string decryption(const string text, const string key) {
 	int index = 0;
 	string result;
 	for (size_t i = 0; i < text.length(); i++, index++) {
 		if (index == key.length())
 			index = 0;
-		if (text[i] + (key[index] - 64) <= 'Z')
-			result += text[i] + (key[index] - 64);
+		if (text[i] - (key[index] - 64) >= 'A')
+			result += text[i] - (key[index] - 64);
 		else
-			result += text[i] + (key[index] - 64) - 26;
+			result += text[i] - (key[index] - 64) + 26;
 	}
 	return result;
 }
 
 int main() {
-	ifstream stringsFile("tj.txt");
-	ifstream keysFile("klucze1.txt");
-	ofstream saveFile("wynik4a.txt");
+	ifstream stringsFile("sz.txt");
+	ifstream keysFile("klucze2.txt");
+	ofstream saveFile("wynik4b.txt");
 	string tempString;
 	string tempKey;
 
-	saveFile << "a)\n";
+	saveFile << "b)\n";
 	while (!stringsFile.eof()) {
 		stringsFile >> tempString;
 		keysFile >> tempKey;
-		saveFile << encryption(tempString, tempKey) << "\n";
+		saveFile << decryption(tempString, tempKey) << "\n";
 	}
 
 	stringsFile.close();
