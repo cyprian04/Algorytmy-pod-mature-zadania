e2927a9e0fd636a00f56b5cb17d542374ec60e89
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-11-26 11:30:39 +0100

    zad 3

--- main.cpp
+++ main.cpp
@@ -1,66 +1,33 @@
 ﻿#include <iostream>
+#include <map>
 using namespace std;
 
-string encryption(string text, string& key) {
-	string result;
-	char vinegret2D[26][26] = {' '};
-	char sign;
+string encryptToMorse(string& text) {
+    string morseCode;
+    map<char, string> morse = {
+        {'A', ".-"}, {'B', "-..."}, {'C', "-.-."},
+        {'D', "-.."}, {'E', "."}, {'F', "..-."},
+        {'G', "--."}, {'H', "...."}, {'I', ".."},
+        {'J', ".---"}, {'K', "-.-"}, {'L', ".-.."},
+        {'M', "--"}, {'N', "-."}, {'O', "---"},
+        {'P', ".--."}, {'Q', "--.-"}, {'R', ".-."},
+        {'S', "..."}, {'T', "-"}, {'U', "..-"},
+        {'V', "...-"}, {'W', ".--"}, {'X', "-..-"},
+        {'Y', "-.--"}, {'Z', "--.."}
+    };
 
-	if (key.length() < text.length()) key += key;
+    for (char ch : text)
+          morseCode += morse[ch] + " ";
 
-	for (int y = 0; y < 26; y++) {
-		sign = 65 + y;
-		for (int x = 0; x < 26; x++) {
-			if (sign > 'Z')
-				sign = 'A';
-			vinegret2D[y][x] = sign;
-			sign++;
-		}
-	}
-	
-	for (size_t i = 0; i < text.length(); i++){
-		int row = text[i] - 65;
-		int column = key[i] - 65;
-		result += vinegret2D[row][column];
-	}
-	return result;
-}
-
-string decryption(string text, string key) {
-	string result;
-	char vinegret2D[26][26] = { ' ' };
-	char sign;
-
-	for (int y = 0; y < 26; y++) {
-		sign = 65 + y;
-		for (int x = 0; x < 26; x++) {
-			if (sign > 'Z')
-				sign = 'A';
-			vinegret2D[y][x] = sign;
-			sign++;
-		}
-	}
-
-	for (size_t i = 0; i < text.length(); i++) {
-		char wantedSign = text[i];
-		int column = key[i] - 65;
-		for (int row = 0; row < 26; row++)
-			if(wantedSign == vinegret2D[row][column])
-				result += vinegret2D[row][0];
-	}
-	return result;
+    return morseCode;
 }
 
 int main() {
-	string key;
-	string text;
+    string text;
+    cout << "Insert text: ";
+    cin >> text;
+    string morseCode = encryptToMorse(text);
+    cout << "Encryptet to: " << morseCode << std::endl;
 
-	cout << "Podaj napis: ";
-	cin >> text;
-	cout << "Podaj klucz: ";
-	cin >> key;
-	string encryptet = encryption(text, key);
-	cout << "\nencryptet to: " << encryptet;
-	cout << "\ndecryptet to: " << decryption(encryptet, key);
-	return 0;
+    return 0;
 }
