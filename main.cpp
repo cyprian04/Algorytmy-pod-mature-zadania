1cfa595b7840bfc0566c1f12ec80af7c2384fc47
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-11-26 11:11:02 +0100

    zad 1

--- main.cpp
+++ main.cpp
@@ -1,35 +1,45 @@
 ﻿#include <iostream>
 using namespace std;
 
-void encryption(string& text, int key) {
-	for (size_t i = 0; i < text.length() - 2; i += 3) {
-		swap(text[i], text[i + 2]);
-		text[i] += 10;
-		text[i + 2] += 10;
-	}
-}
+string encryption(string text, string key) {
+	string result;
+	char vinegret2D[26][26] = {' '};
+	char sign;
 
-void decryption(string& text, int key) {
-	for (size_t i = 0; i < text.length() - 2; i += 3) {
-		swap(text[i], text[i + 2]);
-		text[i] -= 10;
-		text[i + 2] -= 10;
+	if (key.length() < text.length()) key += key;
+
+	for (int y = 0; y < 26; y++) {
+		sign = 65 + y;
+		for (int x = 0; x < 26; x++) {
+			if (sign > 'Z')
+				sign = 'A';
+			vinegret2D[y][x] = sign;
+			sign++;
+		}
 	}
+
+	for (int i = 0; i < 26; i++) {
+		for (int j = 0; j < 26; j++)
+			cout << vinegret2D[i][j] << " ";
+		cout << endl;
+	}
+	
+	for (size_t i = 0; i < text.length(); i++){
+		int row = text[i] - 65;
+		int column = key[i] - 65;
+		result += vinegret2D[row][column];
+	}
+	return result;
 }
 
 int main() {
-	int key = 0;
+	string key;
 	string text;
 
 	cout << "Podaj napis: ";
 	cin >> text;
 	cout << "Podaj klucz: ";
 	cin >> key;
-
-	encryption(text,key);
-	cout << "encryptet to: " << text;
-	decryption(text,key);
-	cout << "\ndecryptet to: " << text;
-
+	cout << "\nencryptet to: " << encryption(text, key);
 	return 0;
 }
