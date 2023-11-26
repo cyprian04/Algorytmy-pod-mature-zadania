0c024792702f05de1b9f92bf44ffc7f54d9ab1ea
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-11-26 12:23:54 +0100

    zad 4

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <sstream>
 #include <map>
 using namespace std;
 
@@ -21,12 +22,39 @@ string encryptToMorse(string text) {
     return result;
 }
 
+string decryptFromMorse(string text) {
+    string signs;
+    string result;
+    map<string, char> reversedMorse = {
+        {".-", 'A'}, {"-...", 'B'}, {"-.-.", 'C'},
+        {"-..", 'D'}, {".", 'E'}, {"..-.", 'F'},
+        {"--.", 'G'}, {"....", 'H'}, {"..", 'I'},
+        {".---", 'J'}, {"-.-", 'K'}, {".-..", 'L'},
+        {"--", 'M'}, {"-.", 'N'}, {"---", 'O'},
+        {".--.", 'P'}, {"--.-", 'Q'}, {".-.", 'R'},
+        {"...", 'S'}, {"-", 'T'}, {"..-", 'U'},
+        {"...-", 'V'}, {".--", 'W'}, {"-..-", 'X'},
+        {"-.--", 'Y'}, {"--..", 'Z'}
+    };
+    istringstream stream(text);
+
+    
+    while (stream >> signs){
+        result += reversedMorse[signs];
+    }
+    return result;
+}
+
+
 int main() {
     string text;
     cout << "Insert text: ";
     cin >> text;
+
     string morseCode = encryptToMorse(text);
     cout << "Encryptet to: " << morseCode <<endl;
 
+    string decryptetCode = decryptFromMorse(morseCode);
+    cout << "Decryptet to: " << decryptetCode << endl;
     return 0;
 }
