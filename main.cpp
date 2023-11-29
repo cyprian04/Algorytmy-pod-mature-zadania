c68d859aa6ad22f5511677ddb1f72794ac40dd9d
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-29 19:46:56 +0100

    poprawione 1

--- main.cpp
+++ main.cpp
@@ -1,129 +1,45 @@
 ﻿#include <iostream>
 using namespace std;
 
-string encryption(int key, string text) {
+string encryption(string text, string key) {
 	string result;
-	char** tablica = new char* [key];
-	for (int i = 0; i < key; i++)
-		tablica[i] = new char[text.length()];
+	char vinegret2D[26][26] = { ' ' };
+	char sign;
 
-	for (int i = 0; i < key; i++)
-		for (int j = 0; j < text.length(); j++)
-			tablica[i][j] = '*';
+	while (key.length() < text.length()) key += key;
 
-
-	int pos = 0;
-	bool goDown = true;
-	for (int i = 0; i < text.length(); i++) {
-
-		if (goDown == true && pos != key - 1) {
-			tablica[pos][i] = text[i];
-			pos++;
-		}
-		else if (goDown == true && pos == key - 1) {
-			tablica[pos][i] = text[i];
-			pos--;
-			goDown = false;
-		}
-		else if (goDown == false && pos != 0) {
-			tablica[pos][i] = text[i];
-			pos--;
-		}
-		else if (goDown == false && pos == 0) {
-			tablica[pos][i] = text[i];
-			pos++;
-			goDown = true;
+	for (int y = 0; y < 26; y++) {
+		sign = 65 + y;
+		for (int x = 0; x < 26; x++) {
+			if (sign > 'Z')
+				sign = 'A';
+			vinegret2D[y][x] = sign;
+			sign++;
 		}
 	}
 
-
-	for (int i = 0; i < key; i++)
-		for (int j = 0; j < text.length(); j++)
-			if (tablica[i][j] != '*')
-				result += tablica[i][j];
-
-	for (int i = 0; i < key; i++)
-		delete[] tablica[i];
-	delete[] tablica;
-
-	return result;
-}
-
-string decryption(int key, string text) {
-	string result;
-	char** tablica = new char* [key];
-	for (int i = 0; i < key; i++)
-		tablica[i] = new char[text.length()];
-
-	for (int i = 0; i < key; i++)
-		for (int j = 0; j < text.length(); j++)
-			tablica[i][j] = '*';
-
-	int pos = 0;
-	bool goDown = true;
-	for (int i = 0; i < text.length(); i++) {
-		tablica[pos][i] = 'X';
-		if (goDown == true && pos != key - 1) {
-			pos++;
-		}
-		else if (goDown == true && pos == key - 1) {
-			pos--;
-			goDown = false;
-		}
-		else if (goDown == false && pos != 0) {
-			pos--;
-		}
-		else if (goDown == false && pos == 0) {
-			pos++;
-			goDown = true;
-		}
+	for (int i = 0; i < 26; i++) {
+		for (int j = 0; j < 26; j++)
+			cout << vinegret2D[i][j] << " ";
+		cout << endl;
 	}
 
-	int index = 0;
-	for (int i = 0; i <key ; i++)
-		for (int j = 0; j <text.length() ; j++)
-			if (tablica[i][j] == 'X')
-				tablica[i][j] = text[index++];
-	pos = 0;
-	goDown = true;
-	for (int i = 0; i < text.length(); i++) {
-		result += tablica[pos][i];
-		if (goDown == true && pos != key - 1) {
-			pos++;
-		}
-		else if (goDown == true && pos == key - 1) {
-			pos--;
-			goDown = false;
-		}
-		else if (goDown == false && pos != 0) {
-			pos--;
-		}
-		else if (goDown == false && pos == 0) {
-			pos++;
-			goDown = true;
-		}
+	for (size_t i = 0; i < text.length(); i++) {
+		int row = text[i] - 65;
+		int column = key[i] - 65;
+		result += vinegret2D[row][column];
 	}
-
-	for (int i = 0; i < key; i++)
-		delete[] tablica[i];
-	delete[] tablica;
-
 	return result;
 }
 
 int main() {
+	string key;
 	string text;
-	int key = 1;
 
 	cout << "Podaj napis: ";
 	cin >> text;
 	cout << "Podaj klucz: ";
 	cin >> key;
-
-	string code = encryption(key, text);
-	cout << "encryptet to: " << code << endl;
-	string decoded = decryption(key, code);
-	cout << "decryptet to: " << decoded;
-	
+	cout << "\nencryptet to: " << encryption(text, key);
 	return 0;
 }
