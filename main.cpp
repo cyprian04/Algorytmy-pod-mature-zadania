c66e71a8fcbf5237da0f2ef25fa4ae58daaba910
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-29 19:49:21 +0100

    poprawione 2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 using namespace std;
 
-string encryption(string text, string key) {
+string encryption(string text, string& key) {
 	string result;
 	char vinegret2D[26][26] = { ' ' };
 	char sign;
@@ -18,16 +18,35 @@ string encryption(string text, string key) {
 		}
 	}
 
-	for (int i = 0; i < 26; i++) {
-		for (int j = 0; j < 26; j++)
-			cout << vinegret2D[i][j] << " ";
-		cout << endl;
-	}
-
 	for (size_t i = 0; i < text.length(); i++) {
 		int row = text[i] - 65;
 		int column = key[i] - 65;
 		result += vinegret2D[row][column];
+	}
+	return result;
+}
+
+string decryption(string text, string key) {
+	string result;
+	char vinegret2D[26][26] = { ' ' };
+	char sign;
+
+	for (int y = 0; y < 26; y++) {
+		sign = 65 + y;
+		for (int x = 0; x < 26; x++) {
+			if (sign > 'Z')
+				sign = 'A';
+			vinegret2D[y][x] = sign;
+			sign++;
+		}
+	}
+
+	for (size_t i = 0; i < text.length(); i++) {
+		char wantedSign = text[i];
+		int column = key[i] - 65;
+		for (int row = 0; row < 26; row++)
+			if (wantedSign == vinegret2D[row][column])
+				result += vinegret2D[row][0];
 	}
 	return result;
 }
@@ -40,6 +59,8 @@ int main() {
 	cin >> text;
 	cout << "Podaj klucz: ";
 	cin >> key;
-	cout << "\nencryptet to: " << encryption(text, key);
+	string encryptet = encryption(text, key);
+	cout << "\nencryptet to: " << encryptet;
+	cout << "\ndecryptet to: " << decryption(encryptet, key);
 	return 0;
 }
