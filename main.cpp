5877b32eade2cf4d8c7861d0c8c6e5fad1686608
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-29 23:26:08 +0100

    zad 6

--- main.cpp
+++ main.cpp
@@ -1,66 +1,145 @@
 ﻿#include <iostream>
 using namespace std;
 
-string encryption(string text, string& key) {
-	string result;
-	char vinegret2D[26][26] = { ' ' };
-	char sign;
+int getTheColumnIndex(char matrix[5][5], char sign) {
+	for (int i = 0; i < 5; i++)
+		for (int j = 0; j < 5; j++)
+			if (sign == matrix[i][j])
+				return j;
+}
 
-	while (key.length() < text.length()) key += key;
+int getTheRowIndex(char matrix[5][5], char sign) {
+	for (int i = 0; i < 5; i++)
+		for (int j = 0; j < 5; j++)
+			if (sign == matrix[i][j])
+				return i;
+}
 
-	for (int y = 0; y < 26; y++) {
-		sign = 65 + y;
-		for (int x = 0; x < 26; x++) {
-			if (sign > 'Z')
-				sign = 'A';
-			vinegret2D[y][x] = sign;
-			sign++;
-		}
-	}
+bool isSignInMatrix(char matrix[5][5] , char sign) {
+	for (int i = 0; i < 5; i++)
+		for (int j = 0; j < 5; j++)
+			if (sign == matrix[i][j])
+				return true;
+	return false;
+}
 
-	for (size_t i = 0; i < text.length(); i++) {
-		int row = text[i] - 65;
-		int column = key[i] - 65;
-		result += vinegret2D[row][column];
-	}
+char putSignIntoMatrix(char matrix[5][5]) {
+	for (char sign = 'A'; sign <= 'Z'; sign++)
+		if (!isSignInMatrix(matrix,sign) && sign !='J')
+			return sign;
+	return '0';
+}
+
+bool isSignInText(string text, char wantedSign) {
+	for (int i = 0; i < text.length(); i++)
+		if (text[i] == wantedSign)
+			return true;
+	return false;
+}
+
+string getTheEcnryptetSignsDiff(char matrix[5][5], char signA, char signB) {
+	string result = "";
+	int signARow = getTheRowIndex(matrix, signA);
+	int signACol = getTheColumnIndex(matrix, signA);
+	int signBRow = getTheRowIndex(matrix, signB);
+	int signBCol = getTheColumnIndex(matrix, signB);
+
+	result += matrix[signBRow][signACol];
+	result += matrix[signARow][signBCol];
 	return result;
 }
 
-string decryption(string text, string key) {
-	string result;
-	char vinegret2D[26][26] = { ' ' };
-	char sign;
+string getTheEcnryptetSignsCol(char matrix[5][5], char signA, char signB) {
+	string result = "";
+	int signARow = getTheRowIndex(matrix, signA);
+	int signACol = getTheColumnIndex(matrix, signA);
+	int signBRow = getTheRowIndex(matrix, signB);
+	int signBCol = getTheColumnIndex(matrix, signB);
 
-	for (int y = 0; y < 26; y++) {
-		sign = 65 + y;
-		for (int x = 0; x < 26; x++) {
-			if (sign > 'Z')
-				sign = 'A';
-			vinegret2D[y][x] = sign;
-			sign++;
+	signARow++;
+	signBRow++;
+
+	if (signARow > 4) signARow = 0;
+	if (signBRow > 4) signBRow = 0;
+	result += matrix[signARow][signACol];
+	result += matrix[signBRow][signBCol];
+
+	return result;
+}
+
+string getTheEcnryptetSignsRow(char matrix[5][5], char signA, char signB) {
+	string result = "";
+	int signARow = getTheRowIndex(matrix, signA);
+	int signACol = getTheColumnIndex(matrix, signA);
+	int signBRow = getTheRowIndex(matrix, signB);
+	int signBCol = getTheColumnIndex(matrix, signB);
+
+	signACol++;
+	signBCol++;
+
+	if (signACol > 4) signACol = 0;
+	if (signBCol > 4) signBCol = 0;
+	result += matrix[signARow][signACol];
+	result += matrix[signBRow][signBCol];
+
+	return result;
+}
+
+string getTheTextWithoutDuplicates(string text) {
+	string distinctText = "";
+	for (int i = 0; i < text.length(); i++)
+		if (!isSignInText(distinctText, text[i]))
+			distinctText += text[i];
+	return distinctText;
+}
+
+string encrypt(string text, string key) {
+	char matrix[5][5];
+	string distinctKey = getTheTextWithoutDuplicates(key);
+	string encryption = "";
+	int counter = 0;
+
+	for (int i = 0; i < 5; i++)
+		for (int j = 0; j < 5; j++)
+			 matrix[i][j] = '0';
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (counter < distinctKey.length()) {
+				matrix[i][j] = distinctKey[counter];
+				counter++;
+			}
+			else matrix[i][j] = putSignIntoMatrix(matrix);
 		}
 	}
 
-	for (size_t i = 0; i < text.length(); i++) {
-		char wantedSign = text[i];
-		int column = key[i] - 65;
-		for (int row = 0; row < 26; row++)
-			if (wantedSign == vinegret2D[row][column])
-				result += vinegret2D[row][0];
+	for (int i = 0; i < text.length() -1; i += 2) {
+		cout << endl << text[i] << " " << text[i + 1] << endl;
+		if (getTheColumnIndex(matrix, text[i]) == getTheColumnIndex(matrix, text[i + 1]))
+			encryption += getTheEcnryptetSignsCol(matrix, text[i], text[i + 1]);
+		else if (getTheRowIndex(matrix, text[i]) == getTheRowIndex(matrix, text[i + 1]))
+			encryption += getTheEcnryptetSignsRow(matrix, text[i], text[i + 1]);
+		else
+			encryption += getTheEcnryptetSignsDiff(matrix, text[i], text[i + 1]);
 	}
-	return result;
+
+	if (text.length() % 2 == 1) 
+		encryption += text[text.length() - 1];
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) 
+			cout << matrix[i][j] << " ";
+		cout << endl;
+	}
+	return encryption;
 }
 
 int main() {
-	string key;
-	string text;
-
-	cout << "Podaj napis: ";
+	string text, key;
+	cout << "Podaj text: ";
 	cin >> text;
 	cout << "Podaj klucz: ";
 	cin >> key;
-	string encryptet = encryption(text, key);
-	cout << "\nencryptet to: " << encryptet;
-	cout << "\ndecryptet to: " << decryption(encryptet, key);
+	cout << text << " encryptet to: " << encrypt(text, key);
 	return 0;
 }
