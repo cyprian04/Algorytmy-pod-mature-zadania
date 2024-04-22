94b514ebf44f293679af94e35b595fd1a332616e
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-22 18:17:03 +0200

    przesuwanie w prawo w tablicy 1D (analogicznie w lewo interacje odwrotnie)

--- main.cpp
+++ main.cpp
@@ -1,22 +1,37 @@
-﻿#include <map>
-#include <string>
-#include <iostream>
+﻿#include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
-	map<char, int> signs;
-	string text;
-	getline(cin, text);
+	int shiftCount = 0;
+	int currentShift = 0;
+	int size = 0;
+	int* arr = nullptr;
 
-	for (auto &s : text)
-		if (s != ' ') {
-			if (s >= 'A' && s <= 'Z')
-				s += 'a' - 'A';
-			signs[s]++;
+	cout << "Podaj ilosc przesuniec: ";
+	cin >> shiftCount;
+	cout << "Podaj rozmiar tablicy: ";
+	cin >> size;
+	arr = new int[size];
+
+	for (int i = 0; i < size; i++) {
+		cout << "Podaj liczbe do tablicy: ";
+		cin >> arr[i];
+	}
+
+	for (int i = size-1; i > 0 && currentShift < shiftCount;){
+		swap(arr[i -1], arr[i]);
+		if (i == 1) {
+			i = size-1;
+			currentShift++;
+			continue;
 		}
+		i--;
+	}
 
-	for (const auto &s : signs)
-		cout << s.first << " wystapilo: " << s.second<<'\n';
+	for (int i = 0; i < size; i++)
+		cout << arr[i] << ", ";
 
+	delete[] arr;
 	return 0;
 }
