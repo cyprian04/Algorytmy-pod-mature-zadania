fd7bc03fa3c4eeac616d453be8653e4f90b3753b
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-22 07:32:10 +0200

    litery alfabetu

--- main.cpp
+++ main.cpp
@@ -1,30 +1,7 @@
 ﻿#include <iostream>
 
-bool isPrime(int num) {
-	for (int i = 2; i < num ; i++)
-		if (num % i == 0)
-			return false;
-	return true && (num != 1);
-}
-
 int main(){
-	int size = 0;
-	int* arr = nullptr;
-
-	std::cout << "Podaj ilosc liczb do sprawdzenia: ";
-	std::cin >> size;
-	arr = new int[size];
-
-	for (int i = 0; i < size; i++){
-		std::cout << "Podaj liczbe " << i + 1 << ":";
-		std::cin >> arr[i];
-	}
-	
-	for (int i = 0; i < size; i++){
-		if (isPrime(arr[i]))
-			std::cout << arr[i] << "Jest liczba pierwsza"<<std::endl;
-		else
-			std::cout << arr[i] << "NIE Jest liczba pierwsza"<<std::endl;
-	}
+	for (char c = 65; c < 91; c++)
+		std::cout << c << " " <<char(c + 32)<<"\n";
 	return 0;
 }
