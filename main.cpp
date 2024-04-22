755245aa52e821706b228c49c0306884ba7aa217
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-22 12:04:20 +0200

    kilka palindromów

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 ﻿#include <iostream>
+#include <vector>
+
+bool isPalindrom(const std::string text_in) {
+	for (int i = 0; i < text_in.length(); i++)
+		if (text_in[i] != text_in[text_in.length() - 1 - i])
+			return false;
+	return true;
+}
 
 int main(){
-	for (char c = 65; c < 91; c++)
-		std::cout << c << " " <<char(c + 32)<<"\n";
+	int vecSize = 0;
+	std::vector<std::string> napisy;
+
+	std::cout << "The number of strings: ";
+	std::cin >> vecSize;
+	for (int i = 0; i < vecSize; i++){
+		std::string text;
+		std::cin >> text;
+		if(isPalindrom(text))
+			napisy.push_back(text);
+	}
+
+	std::cout << '\n';
+	for (const auto& c : napisy)
+		std::cout << c <<'\n';
+
 	return 0;
 }
+
