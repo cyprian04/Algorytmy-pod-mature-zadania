d3122b66adbd07ad78a2e7ca582be59549bf972c
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-23 22:08:52 +0200

    wypełnianie talbicy 2D znakami i podmiana

--- main.cpp
+++ main.cpp
@@ -4,28 +4,16 @@ using namespace std;
 int main() { 
     const int rowsCount = 5;
     const int columnCount = 5;
-    int tab[rowsCount][columnCount];
-    int num = 1;
-    int maxLtoR = 0, maxRtoL = 0;
+    char tab[rowsCount][columnCount];
+    char sign = 'A'; // 65 int value
 
     for (int i = 0; i < rowsCount; i++) {
-        int colNumSum = 0;
         for (int j = 0; j < columnCount; j++) {
-            tab[i][j] = num++;
-            
+            tab[i][j] = sign++;
+            cout << char(tab[i][j] + 32) << " ";
         }
+        cout << endl;
     }
-
-    for (int i = 0; i < rowsCount; i++)
-        if (tab[i][i] > maxLtoR)
-            maxLtoR = tab[i][i];
-
-    for (int i = 0; i < rowsCount; i++)
-        if (tab[i][rowsCount -1 -i] > maxRtoL)
-            maxRtoL = tab[i][rowsCount -1 -i];
-
-    cout << "max na przek. l to r: " << maxLtoR << endl;
-    cout << "max na przek. r to l: " << maxRtoL << endl;
-
+           
     return 0;
  }
