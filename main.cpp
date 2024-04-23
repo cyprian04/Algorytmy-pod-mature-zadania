e64502e6b2817c4d50445e29855a03dba1a0c6a6
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-23 20:31:41 +0200

    wypełnianie tablicy(macierzy)

--- main.cpp
+++ main.cpp
@@ -1,46 +1,19 @@
 ﻿#include <iostream>
-#include <fstream>
 using namespace std;
 
-string MirrorReflection(const string text) {
-    string reflection;
-    for (int i = 0; i < text.length(); i++)
-        reflection += text[text.length() - 1 - i];
-    
-    return reflection;
-}
-
-int countOfText(const string text, fstream& slowa) {
-    int sum = 0;
-    string temp;
-    slowa.open("slowa.txt");
-
-    while (!slowa.eof())
-    {
-        slowa >> temp;
-        if (temp == text) sum++;
-    }
-
-    slowa.close();
-    return sum;
-}
-
 int main() { 
- 
-    string text;
-    fstream nowe;
-    fstream slowa;
-    ofstream saveFile;
-    nowe.open("nowe.txt");
-    saveFile.open("odp6.txt");
+    const int rowsCount = 5;
+    const int columnCount = 5;
+    int tab[rowsCount][columnCount]; // jest macierzą jeśli ta sama liczba rows i col
+    int even = 0;
 
-    while (!nowe.eof()) {
-        nowe >> text;
-        saveFile << text <<" "<< countOfText(text, slowa) <<" ";
-        saveFile << countOfText(MirrorReflection(text), slowa) << endl;
+    for (int i = 0; i < rowsCount; i++)
+        for (int j = 0; j < columnCount; j++)
+            tab[i][j] = (even += 2);
+    for (int i = 0; i < rowsCount; i++) {
+        for (int j = 0; j < columnCount; j++)
+            cout << tab[i][j] << " ";
+        cout << endl;
     }
-
-    nowe.close();
-    saveFile.close();
     return 0;
  }
