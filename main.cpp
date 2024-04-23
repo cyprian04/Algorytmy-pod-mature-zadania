79e145dac06c89fcc3074a584595659a02f44570
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-23 06:11:34 +0200

    największa i najmniejsza suma wartości liczby z pliku

--- main.cpp
+++ main.cpp
@@ -2,28 +2,44 @@
 #include <fstream>
 using namespace std;
 
-bool isPalindrom(const string text) {
-    for (int i = 0; i < text.length(); i++)
-        if (text[i] != text[text.length() - 1 - i])
-            return false;
-
-    return true;
+int sumOfTheNumDigits(const string text) {
+    int sum = 0;
+    for (size_t i = 0; i < text.length(); i++)
+        sum += int(text[i] - 48);
+ 
+    return sum;
 }
 
-int main() {
-    string text;
-    fstream file;
-    ofstream saveFile; // ofstream utworzy nowy plik jeśli ten nie istnieje. sam fstream nic nie utworzy!!!
-    file.open("dane.txt"); // open otwiera plik
-    saveFile.open("odp1.txt");
+int main() { 
+    string maxNum;
+    string minNum;
+    int maxNumSum = 0;
+    int minNumSum = 81;   
 
-    while (!file.eof()) { /// dopóki nie dojdzie do końca pliku
+    string text;
+    ifstream file;
+    ofstream saveFile;
+   
+    file.open("cyfry.txt");
+    saveFile.open("odp2.txt");
+
+    while (!file.eof()) {
         file >> text;
-        if (isPalindrom(text))
-            saveFile << text << endl;
+        int currentNumSum = sumOfTheNumDigits(text);
+
+        if (currentNumSum < minNumSum) {
+            minNumSum = currentNumSum;
+            minNum = text;
+        }
+        if (currentNumSum > maxNumSum) {
+            maxNumSum = currentNumSum;
+            maxNum = text;
+        }
     }
 
-    file.close(); // close zamyka
+    saveFile << "b)\n" << "min: " << minNum << "\nmax: " << maxNum;
+
+    file.close();
     saveFile.close();
     return 0;
-}
+ }
