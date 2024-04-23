45f865903835f24f0093395af8463eb59bff16f2
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-23 20:55:59 +0200

    min i max element w tablicy 2D

--- main.cpp
+++ main.cpp
@@ -4,16 +4,20 @@ using namespace std;
 int main() { 
     const int rowsCount = 5;
     const int columnCount = 5;
-    int tab[rowsCount][columnCount]; // jest macierzą jeśli ta sama liczba rows i col
-    int even = 0;
+    int tab[rowsCount][columnCount];
+    int num = 1;
+    int maxNum = 0, minNum = 0;
 
-    for (int i = 0; i < rowsCount; i++)
-        for (int j = 0; j < columnCount; j++)
-            tab[i][j] = (even += 2);
     for (int i = 0; i < rowsCount; i++) {
-        for (int j = 0; j < columnCount; j++)
-            cout << tab[i][j] << " ";
-        cout << endl;
+        for (int j = 0; j < columnCount; j++) {
+            tab[i][j] = num++;
+            if (tab[i][j] > maxNum)
+                maxNum = tab[i][j];
+            else if (tab[i][j] < minNum)
+                minNum = tab[i][j];
+        }
     }
+    cout << "min: " << minNum << " max: " << maxNum;
+
     return 0;
  }
