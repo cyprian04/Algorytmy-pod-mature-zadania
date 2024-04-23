ba0d6ad8b14bf235ba49251d1ee428799ce996d8
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-23 07:38:47 +0200

    Dla każdego słowa z pliku nowe.txt wypisz to słowo oraz dwie liczby rozdzielone spacją oznaczające:
    • liczbę wystąpień danego słowa w pliku slowa.txt,
    
    • liczbę wystąpień odbicia lustrzanego danego słowa w pliku slowa.txt.

--- main.cpp
+++ main.cpp
@@ -1,26 +1,46 @@
 ﻿#include <iostream>
-#include <map>
 #include <fstream>
 using namespace std;
+
+string MirrorReflection(const string text) {
+    string reflection;
+    for (int i = 0; i < text.length(); i++)
+        reflection += text[text.length() - 1 - i];
+    
+    return reflection;
+}
+
+int countOfText(const string text, fstream& slowa) {
+    int sum = 0;
+    string temp;
+    slowa.open("slowa.txt");
+
+    while (!slowa.eof())
+    {
+        slowa >> temp;
+        if (temp == text) sum++;
+    }
+
+    slowa.close();
+    return sum;
+}
 
 int main() { 
  
     string text;
-    fstream file;
+    fstream nowe;
+    fstream slowa;
     ofstream saveFile;
-    file.open("napisy.txt");
-    saveFile.open("odp5.txt");
-    map<int, int> liczbyK;
+    nowe.open("nowe.txt");
+    saveFile.open("odp6.txt");
 
-    while (!file.eof()) {
-        file >> text;
-        liczbyK[text.length()]++;
+    while (!nowe.eof()) {
+        nowe >> text;
+        saveFile << text <<" "<< countOfText(text, slowa) <<" ";
+        saveFile << countOfText(MirrorReflection(text), slowa) << endl;
     }
 
-    for (const auto c : liczbyK)
-        saveFile << " wyrazów o długości k = " << c.first << " jest ich: " << c.second << endl;
-
-    file.close();
+    nowe.close();
     saveFile.close();
     return 0;
  }
