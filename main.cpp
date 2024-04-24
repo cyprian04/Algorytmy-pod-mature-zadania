be9cdbd6b0a2cb0c16c3c2f21942691719395aae
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-24 07:17:41 +0200

    obracanie tablicy(macierzy) o 90 stopni w prawo

--- main.cpp
+++ main.cpp
@@ -2,15 +2,31 @@
 using namespace std;
 
 int main() { 
-    const int rowsCount = 5;
-    const int columnCount = 5;
-    char tab[rowsCount][columnCount];
-    char sign = 'A'; // 65 int value
+    const int rowsCount = 3;           // 0 1 2     6 3 0  0[0][0] => [0][2]
+    const int columnCount = 3;         // 3 4 5 =>  7 4 1  1[0][1] => [1][2]
+    int tab[rowsCount][columnCount];   // 6 7 8     8 5 2  2[0][2] => [2][2]
+                                                        // 3[1][0] => [0][1]
+                                                        // 4[1][1] => [1][1]
+                                                        // 5[1][2] => [2][1]
+    int num = 0; 
+    int tabPomocnicza[rowsCount][columnCount];
 
     for (int i = 0; i < rowsCount; i++) {
         for (int j = 0; j < columnCount; j++) {
-            tab[i][j] = sign++;
-            cout << char(tab[i][j] + 32) << " ";
+            tab[i][j] = num++;
+            cout << tab[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout <<"\n\n";
+
+    for (int i = 0; i < rowsCount; i++)
+        for (int j = 0; j < columnCount; j++)
+            tabPomocnicza[j][columnCount - 1 - i] = tab[i][j];
+
+    for (int i = 0; i < rowsCount; i++) {
+        for (int j = 0; j < columnCount; j++) {
+            cout << tabPomocnicza[i][j] << " ";
         }
         cout << endl;
     }
