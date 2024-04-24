465711ed0c2a9f81c887de4527360ab41e618634
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-24 11:32:56 +0200

    pattern na skos

--- main.cpp
+++ main.cpp
@@ -3,30 +3,18 @@ using namespace std;
 
 int main() { 
     const int rowsCount = 5;          
-    const int columnCount = 3;         
-    int tab[rowsCount][columnCount];                                     
-    int num = 0; 
-    int tabPomocnicza[columnCount][rowsCount] = {0};
-
+    const int columnCount = 5;         
+    int tab[rowsCount][columnCount] = {0};
     for (int i = 0; i < rowsCount; i++) {
         for (int j = 0; j < columnCount; j++) {
-            tab[i][j] = num++;
+            if (i == j) {
+                tab[i][j] = (i+1) * (j+1);
+            }                
             cout << tab[i][j] << " ";
         }
         cout << endl;
     }
     cout <<"\n\n";
 
-    for (int i = 0; i < columnCount; i++)
-        for (int j = 0; j < rowsCount; j++)
-            tabPomocnicza[i][j] = tab[j][i];
-    
-    for (int i = 0; i < columnCount; i++) {
-        for (int j = 0; j < rowsCount; j++) {
-            cout << tabPomocnicza[i][j] << " ";
-        }
-        cout << endl;
-    }
-           
     return 0;
  }
