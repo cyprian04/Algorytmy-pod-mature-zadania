7f468823e8cf1bb28ebab94cee4efffdb697fdd2
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-24 21:43:03 +0200

    NWW

--- main.cpp
+++ main.cpp
@@ -9,8 +9,10 @@ int nwd(int a, int b) {
         a = b;
         b = change;
     }
-
     return a;
+}
+int nww(int a, int b) {
+    return (a * b) / nwd(a, b);
 }
 
 int main() { 
@@ -19,7 +21,9 @@ int main() {
     cin >> num1;
     cout << "Podaj liczbe b: ";
     cin >> num2;
-    cout << "NWD liczby " << num1 << " i liczby " << num2 << " wynosi: " << nwd(num1, num2);
+    cout << "NWD liczby " << num1 << " i liczby " << num2 << " wynosi: " << nwd(num1, num2) <<endl;
+    cout << "NWW liczby " << num1 << " i liczby " << num2 << " wynosi: " << nww(num1, num2);
+
 
     return 0;
  }
