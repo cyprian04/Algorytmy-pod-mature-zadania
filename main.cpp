c5a1ea585d6b047914610772227d97472a53e24b
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-24 18:38:20 +0200

    dwa wektory jako argumenty i zamieni w nich elementy miejscami, których wartości są liczbami pierwszymi z wektora podanego jako pierwszy argument.
    Przykład: [1,2,17,4] , [5,6,7,8] -> [1,6,7,4] , [5,2,17,8]

--- main.cpp
+++ main.cpp
@@ -2,22 +2,32 @@
 #include <vector>
 using namespace std;
 
+bool isPrime(int num) {
+    for (int i = 2; i < num; i++)
+        if (num % i == 0)
+            return false;
+    return true && num != 1;
+}
+
+void swapPrimeValues(vector<int> &vec1, vector<int> &vec2) {
+    for (int i = 0; i < vec1.size(); i++)
+        if (isPrime(vec1[i]))
+            swap(vec1[i], vec2[i]);
+}
+
 int main() { 
     int num = 0;
     int digit = 0;
-    vector<int>digits;
-    cout << "Podaj liczbe: ";
-    cin >> num;
+    vector<int>nums1 = { 1,2,17,4 };
+    vector<int>nums2 = { 5,6,7,8 };
 
-    while (num != 0){
-        digit = num % 10;
-        digits.emplace_back(digit);
-        num -= digit;
-        num /= 10;
-    }
+    swapPrimeValues(nums1, nums2);
 
-    for (size_t i = 0; i < digits.size(); i++)
-        cout << digits[digits.size() - 1 - i] << " ";
+    for (const auto& n : nums1)
+        cout << n << " ";
+    cout << "\n\n";
+    for (const auto& n : nums2)
+        cout << n << " ";
     
     return 0;
  }
