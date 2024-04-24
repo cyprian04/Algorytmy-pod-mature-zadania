75e24f43530ac310e270751747b3585750608301
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-24 19:40:33 +0200

    Napisz funkcję, która dla danego wektora usunie wszystkie liczby, które są mniejsze od sumy największego i najmniejszego elementu podzielonego przez 2

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
 ﻿#include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int recursiveSum(vector<int>nums, int position){
-    if (position == 0)
-        return nums[0];
+vector<int> filteredSum(vector<int> vec) {
+    vector<int> resultVec;
 
-    return nums[position] + recursiveSum(nums, position - 1);
+    int maxVal = *max_element(vec.begin(), vec.end());
+    int minVal = *min_element(vec.begin(), vec.end());
+    int threshold = (maxVal + minVal) / 2;
+
+    for (int i = 0; i < vec.size(); i++) {
+        if (vec[i] < threshold)
+            resultVec.emplace_back(vec[i]);
+    }
+
+    return resultVec;
 }
 
 int main() { 
-    vector<int>nums = { 1,2,17,4 };
-    cout << recursiveSum(nums, int(nums.size()-1));
+    vector<int>nums = { 1,2,3,4,5,6,7,8,9};
+    vector<int>filtered =  filteredSum(nums);
+
+    for (const auto& n : filtered)
+        cout << n << " ";
+
     return 0;
  }
