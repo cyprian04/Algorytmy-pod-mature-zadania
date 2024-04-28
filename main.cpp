03efc478e8546af47aae051554753a26deacf2f2
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-28 19:15:51 +0200

    bucketSortNegPos (ascending)

--- main.cpp
+++ main.cpp
@@ -2,29 +2,32 @@
 #include <algorithm>
 using namespace std;
 
-void bucketSortAsc(int tab[], int size) {
+void bucketSortNegPos(int tab[], int size) {
     int maxElement = *max_element(tab, tab + size);
-    int* pomocnicza = new int[++maxElement]{ 0 };
+    int minElement = *min_element(tab, tab + size);
+    int newSize = maxElement + 1 - minElement;
+
+    int* pomocnicza = new int[newSize]{ 0 };
     int index = 0;
     for (int k = 0; k < size; k++)
-        pomocnicza[tab[k]]++;
+        pomocnicza[tab[k] - minElement]++;
 
-    for (int i = 0; i < maxElement + 1; i++)
+    for (int i = 0; i < newSize; i++)
         if (pomocnicza[i] != 0)
             for (int j = 0; j < pomocnicza[i]; j++)
-                tab[index++] = i;
+                tab[index++] = i + minElement;
 
     delete[] pomocnicza;
 }
 
 int main() {
     const int size = 10;
-    int tab[size] = { 54, 26, 93, 17, 77, 31, 44, 96, 99, 26};
+    int tab[size] = { -54, 26, -93, 17, 77, 31, -44, 96, 99, 26};
     for (const auto& n : tab)
         cout << n << " ";
 
     cout << "\n\n";
-    bucketSortAsc(tab, size);
+    bucketSortNegPos(tab, size);
 
     for (const auto& n : tab)
         cout << n << " ";
