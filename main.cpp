25f4f8afe7ad4645d6d0585961a942a5608fb46e
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-28 12:28:01 +0200

    anagramy przez sortowanie

--- main.cpp
+++ main.cpp
@@ -2,19 +2,43 @@
 #include <cmath>
 using namespace std;
 
-void bubbleSortDesc(int tab[], int size) {
-    for (int i = 0; i < size - 1; i++)
-        for (int j = 0; j < size - 1; j++)
-            if (tab[j] < tab[j + 1])
-                swap(tab[j], tab[j + 1]);
+void bubbleSortDesc(string &text, size_t size) {
+    for (size_t i = 0; i < size - 1; i++)
+        for (size_t j = 0; j < size - 1; j++)
+            if (text[j] < text[j + 1])
+                swap(text[j], text[j + 1]);
+}
+
+void toUpper(string &text) {
+    for (size_t i = 0; i < text.length(); i++)
+        if (text[i] >= 'a' && text[i] <= 'z')
+            text[i] -= 32;
+}
+
+bool areAnagrams(const string text1, const string text2){
+    if (text1.length() != text2.length())
+        return false;
+
+    string t1 = text1;
+    string t2 = text2;
+    toUpper(t1);
+    toUpper(t2);
+    bubbleSortDesc(t1, t1.length());
+    bubbleSortDesc(t2, t2.length());
+
+    return t1 == t2;
 }
 
 int main() {
-    const int size = 5;
-    int tab[5] = { 8,20,10,15,2 };
-    bubbleSortDesc(tab, size);
+    string text1, text2;
+    cout << "enter text1: ";
+    cin >> text1;
+    cout << "enter text2: ";
+    cin >> text2;
     
-    for (const auto& n : tab)
-        cout << n << " ";
+    if (areAnagrams(text1, text2))
+        cout << "They are anagrams";
+    else
+        cout << "They are not anagrams";
     return 0;
  }
