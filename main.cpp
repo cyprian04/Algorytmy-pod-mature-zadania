d9666d4e1dd0e5010e52ef48499e0298b39ab28c
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-28 10:02:56 +0200

    dopisanie Desc do bubbleSorta

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 
-void bubbleSort(int tab[], int size) {
+void bubbleSortDesc(int tab[], int size) {
     for (int i = 0; i < size - 1; i++)
         for (int j = 0; j < size - 1; j++)
             if (tab[j] < tab[j + 1])
@@ -12,7 +12,7 @@ void bubbleSort(int tab[], int size) {
 int main() {
     const int size = 5;
     int tab[5] = { 8,20,10,15,2 };
-    bubbleSort(tab, size);
+    bubbleSortDesc(tab, size);
     
     for (const auto& n : tab)
         cout << n << " ";
