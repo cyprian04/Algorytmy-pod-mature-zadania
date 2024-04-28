450eb02a8481a2bace27631bbec086b8764a2679
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-28 18:57:20 +0200

    bucketSortAsc

--- main.cpp
+++ main.cpp
@@ -1,31 +1,30 @@
 ﻿#include <iostream>
-#include <cmath>
+#include <algorithm>
 using namespace std;
 
-void insertionSortDesc(int tab[], int size) {
-    for (int i = 0; i < size - 1; i++) {
-        int tempIndex = 0;
-        int index = 0;
-        for (int j = 0; j < i + 1; j++)
-        {
-            if (tab[j] < tab[i + 1]) {
-                tempIndex++;
-                index = j;
-            }
-        }
-        for (int k = index; 0 < tempIndex; tempIndex--,k--)
-            swap(tab[k], tab[k + 1]);
-    }
+void bucketSortAsc(int tab[], int size) {
+    int maxElement = *max_element(tab, tab + size);
+    int* pomocnicza = new int[++maxElement]{ 0 };
+    int index = 0;
+    for (int k = 0; k < size; k++)
+        pomocnicza[tab[k]]++;
+
+    for (int i = 0; i < maxElement + 1; i++)
+        if (pomocnicza[i] != 0)
+            for (int j = 0; j < pomocnicza[i]; j++)
+                tab[index++] = i;
+
+    delete[] pomocnicza;
 }
 
 int main() {
-    const int size = 9;
-    int tab[size] = { 54, 26, 93, 17, 77, 31, 44, 55, 20};
+    const int size = 10;
+    int tab[size] = { 54, 26, 93, 17, 77, 31, 44, 96, 99, 26};
     for (const auto& n : tab)
         cout << n << " ";
 
     cout << "\n\n";
-    insertionSortDesc(tab, size);
+    bucketSortAsc(tab, size);
 
     for (const auto& n : tab)
         cout << n << " ";
