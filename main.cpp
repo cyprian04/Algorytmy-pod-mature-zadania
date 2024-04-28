7def49e155166ccaf6887ff3b8e3b4d404eca0c8
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-28 18:11:57 +0200

    insertionSortDesc

--- main.cpp
+++ main.cpp
@@ -2,25 +2,30 @@
 #include <cmath>
 using namespace std;
 
-void selectionSortDesc(int tab[], int size) {
+void insertionSortDesc(int tab[], int size) {
     for (int i = 0; i < size - 1; i++) {
-        int index = i;
-        for (int j = i + 1; j < size; j++) {
-            if (tab[index] < tab[j])
+        int tempIndex = 0;
+        int index = 0;
+        for (int j = 0; j < i + 1; j++)
+        {
+            if (tab[j] < tab[i + 1]) {
+                tempIndex++;
                 index = j;
+            }
         }
-        swap(tab[i], tab[index]);
+        for (int k = index; 0 < tempIndex; tempIndex--,k--)
+            swap(tab[k], tab[k + 1]);
     }
 }
 
 int main() {
-    const int size = 5;
-    int tab[size] = { 8,20,10,15,2 };
+    const int size = 9;
+    int tab[size] = { 54, 26, 93, 17, 77, 31, 44, 55, 20};
     for (const auto& n : tab)
         cout << n << " ";
 
     cout << "\n\n";
-    selectionSortDesc(tab, size);
+    insertionSortDesc(tab, size);
 
     for (const auto& n : tab)
         cout << n << " ";
