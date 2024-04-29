7ba2570a1ed51c47504f452e3006bc4b9815e20f
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-29 22:05:18 +0200

    Cezar encryption and decryption

--- main.cpp
+++ main.cpp
@@ -1,38 +1,40 @@
 ﻿#include <iostream>
-#include <random>
 using namespace std;
 
-void quickSort(int tab[], int leftIndex, int rightIndex) {
-    if (rightIndex > leftIndex) {
-        cout << "f(" << leftIndex << "," << rightIndex << ")" << endl;
-        random_device rd;
-        mt19937 rng(rd());
-        uniform_int_distribution<int> range(leftIndex, rightIndex);
-
-        int pivot = range(rng);
-        int threshold = leftIndex - 1;
-        int index = leftIndex;
-        swap(tab[pivot], tab[rightIndex]);
-
-        while (leftIndex != rightIndex) {
-            if (tab[leftIndex] < tab[rightIndex])
-                swap(tab[leftIndex], tab[++threshold]);
-            leftIndex++;
-        }        
-        swap(tab[++threshold], tab[rightIndex]);
-
-        quickSort(tab, index, threshold - 1);
-        quickSort(tab, threshold + 1, rightIndex);
+void encryption(string& text, int key) {
+    key %= 26;
+    for (int i = 0; i < text.length(); i++){
+        if (text[i] + key <= 'Z') 
+            text[i] += key;
+        else 
+            text[i] += key - 26;
     }
 }
 
-int main() {
-    int tab[9] = { 8,1,2,3,7,8,2,1,4 };
-    int lewyIndex = 0;
-    int prawyIndex = 8;
-    quickSort(tab, lewyIndex, prawyIndex);
+void decryption(string& text, int key) {
+    key %= 26;
+    for (int i = 0; i < text.length(); i++) {
+        if (text[i] - key >= 'A')
+            text[i] -= key;
+        else
+            text[i] += -key + 26;
+    }
+}
 
-    for (const auto& n: tab)
-        cout << n <<" ";
+
+int main() {
+    string text;
+    int key = 0;
+
+    cout << "Enter text: ";
+    cin >> text;
+    cout << "Enter key: ";
+    cin >> key;
+
+    encryption(text, key);
+    cout << "Encrypted to: " << text << endl;
+    decryption(text, key);
+    cout << "Decrypted to: " << text;
+
     return 0;
  }
