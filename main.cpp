f86c7e09990526c41a5151c3f86f1ab3df29f27d
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-29 19:13:44 +0200

    quickSort where user can choose first pivot index

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
 ﻿#include <iostream>
 using namespace std;
 
-void quickSort(int tab[], int leftIndex, int rightIndex) {
-
+void quickSort(int tab[], int leftIndex, int rightIndex, int pivot_in = -1) {
     if (rightIndex > leftIndex) {
-        cout << "f(" << leftIndex << "," << rightIndex << ")" << endl;
-        int pivot = tab[rightIndex];
         int threshold = leftIndex - 1;
         int index = leftIndex;
 
-        while (leftIndex != rightIndex) {
-            if (tab[leftIndex] < pivot)
-                swap(tab[leftIndex], tab[++threshold]);
-            leftIndex++;
+        if (pivot_in == -1) {
+            cout << "f(" << leftIndex << "," << rightIndex << ")" << endl;
+            int pivot = tab[rightIndex];
+
+            while (leftIndex != rightIndex) {
+                if (tab[leftIndex] < pivot)
+                    swap(tab[leftIndex], tab[++threshold]);
+                leftIndex++;
+            }        
+            swap(tab[++threshold], tab[rightIndex]);
         }
-        swap(tab[++threshold], tab[rightIndex]);
+        else swap(tab[pivot_in], tab[rightIndex]);
 
         quickSort(tab, index, threshold - 1);
         quickSort(tab, threshold + 1, rightIndex);
@@ -25,7 +28,10 @@ int main() {
     int tab[9] = { 8,1,2,3,7,8,2,1,4 };
     int lewyIndex = 0;
     int prawyIndex = 8;
-    quickSort(tab, lewyIndex, prawyIndex);
+    int pivot = 0;
+    cout << "Podaj index pivotu: ";
+    cin >> pivot;
+    quickSort(tab, lewyIndex, prawyIndex, pivot);
 
     for (const auto& n: tab)
         cout << n <<" ";
