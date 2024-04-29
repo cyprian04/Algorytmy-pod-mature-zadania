69d0b355a7c67260e18de03976d04419d40f96ae
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-29 18:12:15 +0200

    quickSortAsc

--- main.cpp
+++ main.cpp
@@ -1,49 +1,31 @@
 ﻿#include <iostream>
 using namespace std;
 
-void merge(int tab[3][4], int column, int lewyIndex, int pivot, int prawyIndex, int pomocnicza[3][4]) {
-    for (int i = lewyIndex; i <= prawyIndex; i++)
-        pomocnicza[i][column] = tab[i][column];
+void quickSort(int tab[], int leftIndex, int rightIndex) {
 
-    int indexLeft = lewyIndex;
-    int indexRight = pivot + 1;
-    int index = lewyIndex;
+    if (rightIndex > leftIndex) {
+        int pivot = tab[rightIndex];
+        int threshold = leftIndex - 1;
 
-    while(indexLeft <= pivot && indexRight <= prawyIndex){
-        if (pomocnicza[indexLeft][column] <= pomocnicza[indexRight][column])
-            tab[index++][column] = pomocnicza[indexLeft++][column];
-        else 
-            tab[index++][column] = pomocnicza[indexRight++][column];
-    }
+        while (leftIndex != rightIndex) {
+            if (tab[leftIndex] < pivot)
+                swap(tab[leftIndex], tab[++threshold]);
+            leftIndex++;
+        }
+        swap(tab[++threshold], tab[rightIndex]);
 
-    while (indexLeft <= pivot)
-        tab[index++][column] = pomocnicza[indexLeft++][column];
-}
-
-void mergeSort(int tab[3][4], int column, int lewyIndex, int prawyIndex, int pomocnicza[3][4]) {
-    if (lewyIndex != prawyIndex) {    
-        int pivot = (lewyIndex + prawyIndex) / 2;
-        mergeSort(tab, column, lewyIndex, pivot, pomocnicza);
-        mergeSort(tab, column, pivot+1, prawyIndex, pomocnicza);
-        merge(tab, column, lewyIndex, pivot, prawyIndex, pomocnicza);
+        quickSort(tab, 0, threshold - 1);
+        quickSort(tab, threshold + 1, rightIndex);
     }
 }
 
 int main() {
-    int matrix[3][4] = { {4, 5, 6, 4},  
-                         {3, 3, 2, 7},  
-                         {1, 1, 1, 1} };
+    int tab[9] = { 8,1,2,3,7,8,2,1,4 };
     int lewyIndex = 0;
-    int prawyIndex = 2;
-    int pomocnicznaTablica[3][4] {0};
+    int prawyIndex = 8;
+    quickSort(tab, lewyIndex, prawyIndex);
 
-    for (int i = 0; i < 4; i++)
-        mergeSort(matrix, i, lewyIndex, prawyIndex, pomocnicznaTablica);
-
-    for (const auto& n : matrix) {
-        for (const auto& k : n)
-            cout << k << " ";
-        cout << endl;
-    }
+    for (const auto& n: tab)
+        cout << n <<" ";
     return 0;
  }
