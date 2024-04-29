39101d1dc80a26f485f5a406835c3801d23bc1d0
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-29 19:35:03 +0200

    pivot is chosen randomly with every quickSort execution

--- main.cpp
+++ main.cpp
@@ -1,23 +1,25 @@
 ﻿#include <iostream>
+#include <random>
 using namespace std;
 
-void quickSort(int tab[], int leftIndex, int rightIndex, int pivot_in = -1) {
+void quickSort(int tab[], int leftIndex, int rightIndex) {
     if (rightIndex > leftIndex) {
+        cout << "f(" << leftIndex << "," << rightIndex << ")" << endl;
+        random_device rd;
+        mt19937 rng(rd());
+        uniform_int_distribution<int> range(leftIndex, rightIndex);
+
+        int pivot = range(rng);
         int threshold = leftIndex - 1;
         int index = leftIndex;
+        swap(tab[pivot], tab[rightIndex]);
 
-        if (pivot_in == -1) {
-            cout << "f(" << leftIndex << "," << rightIndex << ")" << endl;
-            int pivot = tab[rightIndex];
-
-            while (leftIndex != rightIndex) {
-                if (tab[leftIndex] < pivot)
-                    swap(tab[leftIndex], tab[++threshold]);
-                leftIndex++;
-            }        
-            swap(tab[++threshold], tab[rightIndex]);
-        }
-        else swap(tab[pivot_in], tab[rightIndex]);
+        while (leftIndex != rightIndex) {
+            if (tab[leftIndex] < tab[rightIndex])
+                swap(tab[leftIndex], tab[++threshold]);
+            leftIndex++;
+        }        
+        swap(tab[++threshold], tab[rightIndex]);
 
         quickSort(tab, index, threshold - 1);
         quickSort(tab, threshold + 1, rightIndex);
@@ -28,10 +30,7 @@ int main() {
     int tab[9] = { 8,1,2,3,7,8,2,1,4 };
     int lewyIndex = 0;
     int prawyIndex = 8;
-    int pivot = 0;
-    cout << "Podaj index pivotu: ";
-    cin >> pivot;
-    quickSort(tab, lewyIndex, prawyIndex, pivot);
+    quickSort(tab, lewyIndex, prawyIndex);
 
     for (const auto& n: tab)
         cout << n <<" ";
