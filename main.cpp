68ec2881d8b19609a59d04ffea54eeddde33ab54
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-30 14:46:14 +0200

    encryptionANDdecryption Cezar for negative keys as well

--- main.cpp
+++ main.cpp
@@ -1,62 +1,40 @@
 ﻿#include <iostream>
 using namespace std;
 
-void encryptionFence(string& text, int key) {
-    int rows = key;
-    const int columns = int(text.length());
-    char** tab = new char* [rows];
-    for (int i = 0; i < rows; i++)
-        tab[i] = new char[columns];
+void encryption(int key, string& text) {
+	key %= 26;
+	for (size_t i = 0; i < text.length(); i++)
+		if (key < 0 && (text[i] + key) < 'A')
+			text[i] += 26 + key;
+		else if (text[i] + key <= 'Z')
+			text[i] += key;
+		else
+			text[i] += key - 26;
+}
 
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < columns; j++)
-            tab[i][j] = '*';
-
-    bool goingDown = true;
-    int index = 0;
-    for (int i = 0; index != text.length();) {
-        for (int j = 0; j < columns;j++) {
-            tab[i][j] = text[index++];
-
-            if (goingDown && i != rows - 1) i++;
-            else if (goingDown && i == rows - 1) {
-                goingDown = false;
-                i--;
-            }
-            else if (!goingDown && i != 0) i--;
-            else if (!goingDown && i == 0) {
-                goingDown = true;
-                i++;
-            }
-        }
-    }
-    index = 0;
-    for (int i = 0; i < rows; i++) { 
-        for (int j = 0; j < columns; j++) {
-            cout << tab[i][j] << " ";
-            if (tab[i][j] != '*')
-                text[index++] = tab[i][j];
-        }            
-        cout << endl; 
-    }
-
-    for (int i = 0; i < rows; i++)
-           delete[] tab[i];
-    delete[] tab;
+void decryption(int key, string& text) {
+	key %= 26;
+	for (size_t i = 0; i < text.length(); i++)
+		if (key < 0 && (text[i] - key) > 'Z')
+			text[i] += -26 - key;
+		else if (text[i] - key >= 'A')
+			text[i] -= key;
+		else
+			text[i] += -key + 26;
 }
 
 int main() {
-    int key = 2;
-    string text;
-    cout << "Enter text: ";
-    cin >> text;
-    cout << "Enter key: ";
-    cin >> key;
+	string tekst;
+	int key = 0;
 
-    encryptionFence(text, key);
-    cout << "Encrypted to: " << text << endl;
-    //decryption(text);
-    //cout << "Decrypted to: " << text;
+	cout << "Podaj tekst: ";
+	cin >> tekst;
+	cout << "Podaj klucz: ";
+	cin >> key;
 
-    return 0;
- }
+	encryption(key, tekst);
+	cout << "encryptet to: " << tekst;
+	decryption(key, tekst);
+	cout << "\ndecryptet to: " << tekst;
+	return 0;
+}
