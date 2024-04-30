dc01f3d9747c054216e52bcb8fcf84f97c198e72
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-30 09:36:17 +0200

    Szyfr przestawieniowy

--- main.cpp
+++ main.cpp
@@ -1,39 +1,19 @@
 ﻿#include <iostream>
 using namespace std;
 
-void encryption(string& text, int key) {
-    key %= 26;
-    for (int i = 0; i < text.length(); i++){
-        if (text[i] + key <= 'Z') 
-            text[i] += key;
-        else 
-            text[i] += key - 26;
-    }
+void encryptionAndDecryption(string& text) {
+    for (size_t i = 0; i < text.length() - 1; i += 2)
+        swap(text[i], text[i + 1]);
 }
-
-void decryption(string& text, int key) {
-    key %= 26;
-    for (int i = 0; i < text.length(); i++) {
-        if (text[i] - key >= 'A')
-            text[i] -= key;
-        else
-            text[i] += -key + 26;
-    }
-}
-
 
 int main() {
     string text;
-    int key = 0;
-
     cout << "Enter text: ";
     cin >> text;
-    cout << "Enter key: ";
-    cin >> key;
 
-    encryption(text, key);
+    encryptionAndDecryption(text);
     cout << "Encrypted to: " << text << endl;
-    decryption(text, key);
+    encryptionAndDecryption(text);
     cout << "Decrypted to: " << text;
 
     return 0;
