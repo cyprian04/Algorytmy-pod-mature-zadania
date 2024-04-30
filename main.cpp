97cd67c70641f3ac695aee786a680e3e6b853791
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-30 18:27:28 +0200

    encryption and decryption Fence

--- main.cpp
+++ main.cpp
@@ -1,39 +1,127 @@
 ﻿#include <iostream>
-#include <fstream>
 using namespace std;
 
-string decryption(string key, string text) {
-	string result;
-	size_t index = 0;
-	for (size_t i = 0; i < text.length(); i++, index++) {
-		if (index == key.length())
-			index = 0;
-		if (text[i] - (key[index] - 64) >= 'A')
-			result += text[i] - (key[index] - 64);
-		else
-			result += text[i] - (key[index] - 64) + 26;
-	}
-	return result;
+void encryptionFence(string& text, int key) {
+    int rows = key;
+    const int columns = int(text.length());
+    char** tab = new char* [rows];
+    for (int i = 0; i < rows; i++)
+        tab[i] = new char[columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            tab[i][j] = '*';
+
+    bool goingDown = true;
+    int index = 0;
+    for (int i = 0; index != text.length();) {
+        for (int j = 0; j < columns; j++) {
+            tab[i][j] = text[index++];
+
+            if (goingDown && i != rows - 1) i++;
+            else if (goingDown && i == rows - 1) {
+                goingDown = false;
+                i--;
+            }
+            else if (!goingDown && i != 0) i--;
+            else if (!goingDown && i == 0) {
+                goingDown = true;
+                i++;
+            }
+        }
+    }
+    index = 0;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            cout << tab[i][j] << " ";
+            if (tab[i][j] != '*')
+                text[index++] = tab[i][j];
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < rows; i++)
+        delete[] tab[i];
+    delete[] tab;
+}
+
+void decryptionFence(string& text, int key) {
+    int rows = key;
+    const int columns = int(text.length());
+    char** tab = new char* [rows];
+    for (int i = 0; i < rows; i++)
+        tab[i] = new char[columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            tab[i][j] = '*';
+
+    int index = 0;
+    bool goingDown = true;
+    for (int i = 0; index != text.length();) {
+        for (int j = 0; j < columns; j++, index++) {
+            tab[i][j] = 'X';
+
+            if (goingDown && i != rows - 1) i++;
+            else if (goingDown && i == rows - 1) {
+                goingDown = false;
+                i--;
+            }
+            else if (!goingDown && i != 0) i--;
+            else if (!goingDown && i == 0) {
+                goingDown = true;
+                i++;
+            }
+        }
+    }
+
+    index = 0;
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            if (tab[i][j] == 'X')
+                 tab[i][j] = text[index++];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++)
+            cout << tab[i][j] << " ";
+        cout << endl;
+    }
+
+    index = 0;
+    for (int i = 0; index != text.length();) {
+        for (int j = 0; j < columns; j++) {
+            text[index++] = tab[i][j];
+
+            if (goingDown && i != rows - 1) i++;
+            else if (goingDown && i == rows - 1) {
+                goingDown = false;
+                i--;
+            }
+            else if (!goingDown && i != 0) i--;
+            else if (!goingDown && i == 0) {
+                goingDown = true;
+                i++;
+            }
+        }
+    }
+
+    for (int i = 0; i < rows; i++)
+        delete[] tab[i];
+    delete[] tab;
 }
 
 int main() {
-	fstream file;
-	fstream keys;
-	ofstream saveFile;
-	file.open("sz.txt");
-	keys.open("klucze2.txt");
-	saveFile.open("wynik5b.txt");
+    int key = 2;
+    string text;
+    cout << "Enter text: ";
+    cin >> text;
+    cout << "Enter key: ";
+    cin >> key;
 
-	string text;
-	string key;
-	while (!file.eof()){
-		file >> text;
-		keys >> key;
-		saveFile << decryption(key, text) << endl;
-	}
+    encryptionFence(text, key);
+    cout << "Encrypted to: " << text << endl;
+    decryptionFence(text, key);
+    cout << "Decrypted to: " << text;
 
-	file.close();
-	keys.close();
-	saveFile.close();
-	return 0;
+    return 0;
 }
