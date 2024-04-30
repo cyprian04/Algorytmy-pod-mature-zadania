0e8b66242e38ec3473bc3743be912319ce340c85
Cyprian Gibas <cyprian.gibas@o2.pl>
2024-04-30 16:42:48 +0200

    My first version of this task on files

--- main.cpp
+++ main.cpp
@@ -1,30 +1,46 @@
 ﻿#include <iostream>
-#include <sstream>
+#include <fstream>
 using namespace std;
 
-string encryptionAndDecryptionBackwards(istringstream& text) {
-	string result;
-	string temp;
-	while (text >> temp) {
-		for (int i = 0; i < temp.length(); i++){
-			result += temp[temp.length() - 1 - i];
+void CheckAndFixLength(string& key, const string text) {
+	if (key.length() < text.length()) {
+		int transitions = int(text.length() - key.length());
+		for (int i = 0; 0 < transitions; transitions--){
+				key += key[i++];
+				if (i == key.length()) i = 0;
 		}
-		result += ' ';
 	}
-	return result;
+}
+
+string encryption(string key, string& text) {
+	for (size_t i = 0; i < text.length(); i++){
+		if (text[i] + (key[i] - 64)<= 'Z')
+			text[i] += (key[i] - 64);
+		else
+			text[i] += (key[i] - 64) - 26;
+	}
+	return text;
 }
 
 int main() {
-	string tekst;
-	cout << "Podaj tekst: ";
+	fstream file;
+	fstream keys;
+	ofstream saveFile;
+	file.open("tj.txt");
+	keys.open("klucze1.txt");
+	saveFile.open("wynik5a.txt");
 
-	getline(cin, tekst);
-	istringstream stream1(tekst);
-	tekst = encryptionAndDecryptionBackwards(stream1);
-	cout << "encryptet to: " << tekst;
+	string text;
+	string key;
+	while (!file.eof()){
+		file >> text;
+		keys >> key;
+		CheckAndFixLength(key, text);
+		saveFile << encryption(key, text) <<endl;
+	}
 
-	istringstream stream2(tekst);
-	tekst = encryptionAndDecryptionBackwards(stream2);
-	cout << "\ndecryptet to: " << tekst;
+	file.close();
+	keys.close();
+	saveFile.close();
 	return 0;
 }
